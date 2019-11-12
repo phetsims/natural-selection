1b2fec125a9122f708741afdd974977402e15722
Chris Malley <cmalley@pixelzoom.com>
2019-11-12 10:47:19 -0700

    support Rectangle API for HatchingRectangle

--- js/common/view/HatchingRectangle.js
+++ js/common/view/HatchingRectangle.js
@@ -46,21 +46,47 @@ define( require => {
 
       super( 0, 0, width, height, options );
 
+      // @private
+      this.hatchingLineWidth = options.hatchingOptions.lineWidth;
+
+      // @private
+      this.linesPath = new Path( new Shape(), options.hatchingOptions );
+      this.addChild( this.linesPath );
+
+      // this.linesPath did not exist the first time that invalidateRectangle was called by super.
+      this.invalidateRectangle();
+    }
+
+    /**
+     * Notifies that the rectangle has changed, and invalidates path information and our cached shape.
+     * @protected
+     * @override
+     */
+    invalidateRectangle() {
+      super.invalidateRectangle();
+
+      // this.linesPath did not exist the first time that invalidateRectangle was called by super.
+      if ( this.linesPath ) {
+
+        // Draw equally-spaces lines on top of the rectangle to create a hatching pattern. The lines are drawn
+        // horizontally as a single Shape, then rotated to the desired angle. The bounds of the lines is 2x the
+        // largest dimensions of the rectangle, so that the hatching pattern can be rotated arbitrarily.
+        let lineY = 0;
+        const linesShape = new Shape();
+        const maxDimension = Math.max( this.rectWidth, this.rectHeight );
+        while ( lineY <= 2 * maxDimension ) {
+          linesShape.moveTo( 0, lineY ).lineTo( 2 * maxDimension, lineY );
+          lineY = lineY + 2 * this.hatchingLineWidth;
+        }
+        this.linesPath.setShape( linesShape );
+
+        // Center hatching lines in the rectangle
+        this.linesPath.centerX = this.rectX + this.rectWidth / 2;
+        this.linesPath.centerY = this.rectY + this.rectHeight / 2;
+      }
+
       // Clip to the shape of the rectangle
       this.clipArea = this.shape;
-
-      // Draw equally-spaces lines on top of the rectangle to create a hatching pattern. The lines are drawn
-      // horizontally as a single Shape, then rotated to the desired angle. The bounds of the lines is 2x the
-      // dimensions of the rectangle, so that the hatching pattern can be rotated arbitrarily.
-      let lineY = 0;
-      const linesShape = new Shape();
-      while ( lineY <= 2 * height ) {
-        linesShape.moveTo( 0, lineY ).lineTo( 2 * width, lineY );
-        lineY = lineY + 2 * options.hatchingOptions.lineWidth;
-      }
-      const linesPath = new Path( linesShape, options.hatchingOptions );
-      linesPath.center = this.center;
-      this.addChild( linesPath );
     }
   }
 
