695b6e1842ff4e2917d256605fdced0d6ee57214
Chris Malley <cmalley@pixelzoom.com>
2019-11-12 11:18:40 -0700

    add % to Proportions

--- js/common/view/ProportionBarNode.js
+++ js/common/view/ProportionBarNode.js
@@ -14,8 +14,18 @@ define( require => {
   const HatchingRectangle = require( 'NATURAL_SELECTION/common/view/HatchingRectangle' );
   const naturalSelection = require( 'NATURAL_SELECTION/naturalSelection' );
   const Node = require( 'SCENERY/nodes/Node' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const Property = require( 'AXON/Property' );
   const Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
+  const Text = require( 'SCENERY/nodes/Text' );
+  const Util = require( 'DOT/Util' );
+
+  // strings
+  const valuePercentString = require( 'string!NATURAL_SELECTION/valuePercent' );
+
+  // constants
+  const PERCENTAGE_FONT = new PhetFont( 12 );
 
   class ProportionBarNode extends Node {
 
@@ -32,22 +42,32 @@ define( require => {
         barHeight: 30
       }, options );
 
+      // Portions of the bar for non-mutant and mutant counts
       const nonMutantRectangle = new Rectangle( 0, 0, options.barWidth, options.barHeight, {
         fill: color
       } );
-
       const mutantRectangle = new HatchingRectangle( 0, 0, options.barWidth, options.barHeight, {
         fill: color
       } );
 
+      // Percentages for non-mutant and mutant counts
+      const percentageOptions = {
+        font: PERCENTAGE_FONT,
+        bottom: -4
+      };
+      const nonMutantPercentageNode = new Text( '', percentageOptions );
+      const mutantPercentageNode = new Text( '', percentageOptions );
+
       assert && assert( !options.children, 'ProportionBarNode sets children' );
-      options.children = [ nonMutantRectangle, mutantRectangle ];
+      options.children = [ nonMutantRectangle, mutantRectangle, nonMutantPercentageNode, mutantPercentageNode ];
 
       super( options );
 
       // @private
       this.nonMutantRectangle = nonMutantRectangle;
       this.mutantRectangle = mutantRectangle;
+      this.nonMutantPercentageNode = nonMutantPercentageNode;
+      this.mutantPercentageNode = mutantPercentageNode;
       this.barWidth = options.barWidth;
 
       Property.multilink( [ nonMutantCountCountProperty, mutantCountProperty ],
@@ -65,13 +85,31 @@ define( require => {
 
       const total = nonMutantCountCount + mutantCount;
 
-      //TODO round to integer values
+      //TODO display 1 > value > 0 as 1%
       const nonMutantPercentage = nonMutantCountCount / total;
       const mutantPercentage = mutantCount / total;
 
-      this.nonMutantRectangle.visible = ( nonMutantPercentage > 0 );
-      this.mutantRectangle.visible = ( mutantPercentage > 0 );
+      // update %
+      this.nonMutantPercentageNode.text = StringUtils.fillIn( valuePercentString, {
+        value: Util.roundSymmetric( 100 * nonMutantPercentage )
+      } );
+      this.mutantPercentageNode.text = StringUtils.fillIn( valuePercentString, {
+        value: Util.roundSymmetric( 100 *  mutantPercentage )
+      } );
 
+      // center % in its portion of the bar
+      if ( nonMutantPercentage > 0 ) {
+        this.nonMutantPercentageNode.centerX = nonMutantPercentage * this.barWidth / 2;
+      }
+      if ( mutantPercentage > 0 ) {
+        this.mutantPercentageNode.centerX = this.barWidth - ( mutantPercentage * this.barWidth / 2 );
+      }
+
+      // if either count is zero, hide its bar and %
+      this.nonMutantRectangle.visible = this.nonMutantPercentageNode.visible = ( nonMutantPercentage > 0 );
+      this.mutantRectangle.visible = this.mutantPercentageNode.visible = ( mutantPercentage > 0 );
+
+      // update the mutant portion of the bar
       if ( mutantPercentage > 0 ) {
         this.mutantRectangle.rectWidth = mutantPercentage * this.barWidth;
         this.mutantRectangle.right = this.nonMutantRectangle.right;
