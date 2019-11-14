c3a32263d00aee14454745c812be4ad835ee6597
Chris Malley <cmalley@pixelzoom.com>
2019-11-14 10:05:05 -0700

    delete vestigial view Properties

--- js/lab/view/LabViewProperties.js
+++ js/lab/view/LabViewProperties.js
@@ -9,7 +9,6 @@ define( require => {
   'use strict';
 
   // modules
-  const BooleanProperty = require( 'AXON/BooleanProperty' );
   const naturalSelection = require( 'NATURAL_SELECTION/naturalSelection' );
   const NaturalSelectionViewProperties = require( 'NATURAL_SELECTION/common/view/NaturalSelectionViewProperties' );
 
@@ -17,19 +16,7 @@ define( require => {
 
     constructor() {
       super();
-
-      // @public visibility of traits on the Population graph
-      this.populationWhiteFurVisibleProperty = new BooleanProperty( false );
-      this.populationBrownFurVisibleProperty = new BooleanProperty( false );
-      this.populationTallEarsVisibleProperty = new BooleanProperty( false );
-      this.populationFlatEarsVisibleProperty = new BooleanProperty( false );
-      this.populationShortTeethVisibleProperty = new BooleanProperty( false );
-      this.populationLongTeethVisibleProperty = new BooleanProperty( false );
-
-      // @public visibility of alleles on the Pedigree graph
-      this.furAllelesVisibleProperty = new BooleanProperty( false );
-      this.earAllelesVisibleProperty = new BooleanProperty( false );
-      this.teethAllelesVisibleProperty = new BooleanProperty( false );
+      //TODO
     }
 
     /**
@@ -38,15 +25,7 @@ define( require => {
      */
     reset() {
       super.reset();
-      this.populationWhiteFurVisibleProperty.reset();
-      this.populationBrownFurVisibleProperty.reset();
-      this.populationTallEarsVisibleProperty.reset();
-      this.populationFlatEarsVisibleProperty.reset();
-      this.populationShortTeethVisibleProperty.reset();
-      this.populationLongTeethVisibleProperty.reset();
-      this.furAllelesVisibleProperty.reset();
-      this.earAllelesVisibleProperty.reset();
-      this.teethAllelesVisibleProperty.reset();
+      //TODO
     }
   }
 
