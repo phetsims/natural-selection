6edc373ef55106165fedfed0f754111624086f37
Chris Malley <cmalley@pixelzoom.com>
2019-11-26 15:51:11 -0700

    PANEL_STROKE = SCREEN_VIEW_BACKGROUND.darkerColor

--- js/common/NaturalSelectionColors.js
+++ js/common/NaturalSelectionColors.js
@@ -2,25 +2,28 @@
 
 /**
  * Colors used in this sim.
- * 
+ *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 define( require => {
   'use strict';
 
   // modules
+  const Color = require( 'SCENERY/util/Color' );
   const naturalSelection = require( 'NATURAL_SELECTION/naturalSelection' );
   const PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
+
+  const SCREEN_VIEW_BACKGROUND = new Color( 220, 231, 184 );
 
   // Panel-like things are stroked with a darker version of SCREEN_VIEW_BACKGROUND. This provides a clear, but
   // subtle, border around the panels, and avoids the rectangles-inside-of-rectangles look that is typical of
   // the default 'black' stroke.
-  const PANEL_STROKE = 'rgb( 180, 190, 140 )';
+  const PANEL_STROKE = SCREEN_VIEW_BACKGROUND.darkerColor( 0.82 );
 
   const NaturalSelectionColors = {
 
     // ScreenView
-    SCREEN_VIEW_BACKGROUND: 'rgb( 220, 231, 184 )',
+    SCREEN_VIEW_BACKGROUND: SCREEN_VIEW_BACKGROUND,
 
     // ViewportNode
     VIEWPORT_NODE_STROKE: PANEL_STROKE,
