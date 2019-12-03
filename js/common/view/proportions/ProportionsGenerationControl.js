30adea1ce32471b4202ee4c1ad98ad06aaae7215
pixelzoom <cmalley@pixelzoom.com>
2019-12-03 13:37:24 -0700

    use NumberDisplay in generation control

--- js/common/view/proportions/ProportionsGenerationControl.js
+++ js/common/view/proportions/ProportionsGenerationControl.js
@@ -11,16 +11,14 @@ define( require => {
   // modules
   const ArrowButton = require( 'SUN/buttons/ArrowButton' );
   const HBox = require( 'SCENERY/nodes/HBox' );
-  const HStrut = require( 'SCENERY/nodes/HStrut' );
   const merge = require( 'PHET_CORE/merge' );
   const naturalSelection = require( 'NATURAL_SELECTION/naturalSelection' );
   const NaturalSelectionConstants = require( 'NATURAL_SELECTION/common/NaturalSelectionConstants' );
-  const Node = require( 'SCENERY/nodes/Node' );
-  const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
-  const Text = require( 'SCENERY/nodes/Text' );
+  const NumberDisplay = require( 'SCENERY_PHET/NumberDisplay' );
+  const Range = require( 'DOT/Range' );
 
   // strings
-  const generationNumberString = require( 'string!NATURAL_SELECTION/generationNumber' );
+  const generationValueString = require( 'string!NATURAL_SELECTION/generationValue' );
 
   class ProportionsGenerationControl extends HBox {
 
@@ -34,44 +32,37 @@ define( require => {
         spacing: 10
       }, options );
 
-      // Previous button decrements the generation number
-      const previous = () => generationProperty.value--;
-      const previousButton = new ArrowButton( 'left', previous, NaturalSelectionConstants.ARROW_BUTTON_OPTIONS );
+      // Previous button, decrements the generation number
+      const previousButton = new ArrowButton( 'left',
+        () => generationProperty.value--,
+        NaturalSelectionConstants.ARROW_BUTTON_OPTIONS
+      );
 
-      // Next button decrements the generation number
-      const next = () => generationProperty.value++;
-      const nextButton = new ArrowButton( 'right', next, NaturalSelectionConstants.ARROW_BUTTON_OPTIONS );
+      // Next button, increments the generation number
+      const nextButton = new ArrowButton( 'right',
+        () => generationProperty.value++,
+        NaturalSelectionConstants.ARROW_BUTTON_OPTIONS
+      );
 
       // Generation number display
-      const generationText = new Text( StringUtils.fillIn( generationNumberString, {
-        number: 99 // values greater than this will result in generationText being scaled down 
-      } ), {
+      const generationDisplay = new NumberDisplay( generationProperty, new Range( 0, 99 ), {
+        align: 'center',
+        valuePattern: generationValueString,
         font: NaturalSelectionConstants.PROPORTIONS_GENERATION_CONTROL_FONT,
+        xMargin: 0,
+        yMargin: 0,
+        backgroundFill: null,
+        backgroundStroke: null,
         maxWidth: 150 // determined empirically
       } );
 
-      // This bit of code allows us to scale generationText and keep it centered between the buttons. 
-      // We initialized generationText with a big generation number, so that becomes its new maxWidth.
-      // The strut is sized to this maxWidth, and will be used to keep the display centered.
-      generationText.maxWidth = generationText.width;
-      const hStrut = new HStrut( generationText.width );
-      const displayNode = new Node( {
-        children: [ hStrut, generationText ]
-      } );
-
       assert && assert( !options.children, 'ProportionsGenerationControl sets children' );
-      options.children = [ previousButton, displayNode, nextButton ];
+      options.children = [ previousButton, generationDisplay, nextButton ];
 
       super( options );
 
+      // Enable buttons
       generationProperty.link( generation => {
-
-        // Update the generation number and re-center it
-        generationText.text = StringUtils.fillIn( generationNumberString, { number: generation } );
-        generationText.centerX = hStrut.centerX;
-        generationText.centerY = previousButton.centerY;
-
-        // Enable buttons
         previousButton.enabled = ( generation > 0 );
         //TODO nextButton.enabled = ?
       } );
