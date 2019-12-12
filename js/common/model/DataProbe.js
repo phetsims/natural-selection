77042df2f4d63b16e667f82c02cec198a39ba00a
pixelzoom <cmalley@pixelzoom.com>
2019-12-12 16:11:47 -0700

    TODO

--- js/common/model/DataProbe.js
+++ js/common/model/DataProbe.js
@@ -32,6 +32,7 @@ define( require => {
         tandem: tandem.createTandem( 'visibleProperty' )
       } );
 
+      //TODO add rangeProperty or set phetioStudioControl:false
       // @public the generation (x) value
       this.generationProperty = new NumberProperty( 0, {
         tandem: tandem.createTandem( 'generationProperty' )
