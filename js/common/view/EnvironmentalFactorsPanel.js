b8102b665c362dd874df34282779bf4386e5569d
phet-dev <phetadmin@gmail.com>
2020-01-23 03:48:44 -0700

    update copyright dates from daily grunt work

--- js/common/view/EnvironmentalFactorsPanel.js
+++ js/common/view/EnvironmentalFactorsPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * EnvironmentalFactorsPanel is the panel that contains controls for environmental factors that affect
