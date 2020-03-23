a465f1b1946c654b59de4bb71850649cc8a0a9c8
pixelzoom <cmalley@pixelzoom.com>
2020-03-23 16:11:43 -0600

    doc

--- js/common/NaturalSelectionQueryParameters.js
+++ js/common/NaturalSelectionQueryParameters.js
@@ -33,32 +33,32 @@ const NaturalSelectionQueryParameters = QueryStringMachine.getAll( {
   // Specifies the mutations that appear in the initial population of bunnies. The value determines which
   // alleles abbreviations can appear in the population query parameter.
   // See https://github.com/phetsims/natural-selection/issues/9 for design history and specification.
-  //TODO how to specify mutations per screen?
+  //TODO https://github.com/phetsims/natural-selection/issues/9 how to specify mutations per screen?
   mutations: {
     type: 'array',
     elementSchema: {
       type: 'string'
     },
     defaultValue: [],
-    isValidValue: mutations => true, //TODO #9
+    isValidValue: mutations => true, //TODO https://github.com/phetsims/natural-selection/issues/9
     public: true
   },
 
   // Specifies the initial population of bunnies. The value of the mutations query parameter determines which
   // alleles abbreviations can appear in this query parameter's value.
   // See https://github.com/phetsims/natural-selection/issues/9 for design history and specification.
-  //TODO how to specify population per screen?
+  //TODO https://github.com/phetsims/natural-selection/issues/9 how to specify population per screen?
   // population: {
   //   type: 'array',
   //   elementSchema: {
   //     type: 'string'
   //   },
   //   defaultValue: [ '1' ],
-  //   isValidValue: population => true, //TODO #9
+  //   isValidValue: population => true, //TODO https://github.com/phetsims/natural-selection/issues/9
   //   public: true
   // },
 
-  //TODO #9 for now, ignore mutations and specify the initial number of bunnies
+  //TODO https://github.com/phetsims/natural-selection/issues/9 for now, ignore mutations and specify the initial number of bunnies
   population: {
     type: 'number',
     defaultValue: 1,
@@ -98,7 +98,7 @@ const NaturalSelectionQueryParameters = QueryStringMachine.getAll( {
 // check dependencies between query parameters
 
 // Check that population is valid for mutations, see https://github.com/phetsims/natural-selection/issues/9
-//TODO #9, validate mutations and population, call QueryStringMachine.addWarning
+//TODO https://github.com/phetsims/natural-selection/issues/49, validate mutations and population, call QueryStringMachine.addWarning
 
 assert && assert( NaturalSelectionQueryParameters.secondsPerStep < NaturalSelectionQueryParameters.secondsPerGeneration,
   'secondsPerStep must be < secondsPerGeneration' );
