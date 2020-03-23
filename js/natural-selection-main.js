dfcfc6249858437525e3b8edc0ae857145403efc
pixelzoom <cmalley@pixelzoom.com>
2020-03-23 13:14:59 -0600

    delete string constants

--- js/natural-selection-main.js
+++ js/natural-selection-main.js
@@ -13,24 +13,24 @@ import IntroScreen from './intro/IntroScreen.js';
 import LabScreen from './lab/LabScreen.js';
 import naturalSelectionStrings from './natural-selection-strings.js';
 
-const naturalSelectionTitleString = naturalSelectionStrings[ 'natural-selection' ].title;
-
-const simOptions = {
-  //TODO https://github.com/phetsims/natural-selection/issues/2 complete the credits
-  credits: {
-    leadDesign: 'Amanda McGarry',
-    softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
-    team: 'Wendy Adams, Megan Hoffman, Oliver Nix, Jonathan Olson, Ariel Paul, Kathy Perkins, Noah Podolefsky, Carl Wieman',
-    qualityAssurance: 'Katie Woessner',
-    graphicArts: 'Megan Lai'
-  }
-};
-
 SimLauncher.launch( () => {
+
   const screens = [
     new IntroScreen( Tandem.ROOT.createTandem( 'introScreen' ) ),
     new LabScreen( Tandem.ROOT.createTandem( 'labScreen' ) )
   ];
-  const sim = new Sim( naturalSelectionTitleString, screens, simOptions );
+
+  const sim = new Sim( naturalSelectionStrings[ 'natural-selection' ].title, screens, {
+
+    //TODO https://github.com/phetsims/natural-selection/issues/2 complete the credits
+    credits: {
+      leadDesign: 'Amanda McGarry',
+      softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
+      team: 'Wendy Adams, Megan Hoffman, Oliver Nix, Jonathan Olson, Ariel Paul, Kathy Perkins, Noah Podolefsky, Carl Wieman',
+      qualityAssurance: 'Katie Woessner',
+      graphicArts: 'Megan Lai'
+    }
+  } );
+
   sim.start();
 } );
