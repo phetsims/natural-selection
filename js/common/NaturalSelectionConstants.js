ba02ffc241d6bf367072121e39e099e09ca2b5c0
pixelzoom <cmalley@pixelzoom.com>
2020-04-02 15:18:56 -0600

    doc

--- js/common/NaturalSelectionConstants.js
+++ js/common/NaturalSelectionConstants.js
@@ -1,7 +1,7 @@
 // Copyright 2019-2020, University of Colorado Boulder
 
 /**
- * Constants used throughout within this simulation.
+ * Constants used throughout this simulation.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
