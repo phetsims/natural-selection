7eecce6cd79c01dcd7daac55bd92bc497f732330
pixelzoom <cmalley@pixelzoom.com>
2020-05-07 11:47:34 -0600

    revise phetioDocumentation for simulationModeProperty

--- js/common/model/NaturalSelectionModel.js
+++ js/common/model/NaturalSelectionModel.js
@@ -44,7 +44,7 @@ class NaturalSelectionModel {
     // @public
     this.simulationModeProperty = new EnumerationProperty( SimulationMode, SimulationMode.STAGED, {
       tandem: tandem.createTandem( 'simulationModeProperty' ),
-      phetioDocumentation: 'do not use this Property, it is not part of the public PhET-iO API',
+      phetioDocumentation: 'for internal use only',
       phetioReadOnly: true
     } );
 
