2765d1c3d0d9d2155351ace748c6027dcb7f1dc1
pixelzoom <cmalley@pixelzoom.com>
2020-05-21 10:19:20 -0600

    doc

--- js/natural-selection-main.js
+++ js/natural-selection-main.js
@@ -1,7 +1,7 @@
 // Copyright 2019-2020, University of Colorado Boulder
 
 /**
- * Main entry point for the sim.
+ * Main entry point for the simulation.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
