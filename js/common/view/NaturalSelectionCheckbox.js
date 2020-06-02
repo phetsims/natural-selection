6be3dcb630fe9b379ffb1671fb3b561180245ba0
pixelzoom <cmalley@pixelzoom.com>
2020-06-02 15:18:34 -0600

    delete unused feature from NaturalSelectionCheckbox, #92

--- js/common/view/NaturalSelectionCheckbox.js
+++ js/common/view/NaturalSelectionCheckbox.js
@@ -2,14 +2,12 @@
 
 /**
  * NaturalSelectionCheckbox is the checkbox subclass used throughout this sim.
- * In addition to styling the checkbox for this sim (via options), it provides an optional AlignGroup for its label,
- * which can be used to make a set of checkboxes all have the same effective size.
+ * It styles the checkbox for this sim (via options).
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import merge from '../../../../phet-core/js/merge.js';
-import AlignBox from '../../../../scenery/js/nodes/AlignBox.js';
 import Checkbox from '../../../../sun/js/Checkbox.js';
 import naturalSelection from '../../naturalSelection.js';
 import NaturalSelectionConstants from '../NaturalSelectionConstants.js';
@@ -17,22 +15,13 @@ import NaturalSelectionConstants from '../NaturalSelectionConstants.js';
 class NaturalSelectionCheckbox extends Checkbox {
 
   /**
-   * @param {Node} labelNode
+   * @param {Node} content
    * @param {Property.<boolean>} property
    * @param {Object} [options]
    */
-  constructor( labelNode, property, options ) {
+  constructor( content, property, options ) {
 
-    options = merge( {
-      labelAlignGroup: null // {AlignGroup|null},
-    }, NaturalSelectionConstants.CHECKBOX_OPTIONS, options );
-
-    const content = options.labelAlignGroup ?
-                    new AlignBox( labelNode, {
-                      group: options.labelAlignGroup,
-                      xAlign: 'left'
-                    } ) :
-                    labelNode;
+    options = merge( {}, NaturalSelectionConstants.CHECKBOX_OPTIONS, options );
 
     super( content, property, options );
   }
