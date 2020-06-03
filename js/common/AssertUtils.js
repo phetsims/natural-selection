3cb4822ab0f2919ff6d868976ed3b6a5f407533b
phet-dev <phetadmin@gmail.com>
2020-06-03 03:20:46 -0600

    update copyright dates from daily grunt work

--- js/common/AssertUtils.js
+++ js/common/AssertUtils.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
 //TODO https://github.com/phetsims/natural-selection/issues/96 migrate to common?
 /**
