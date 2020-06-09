515bf1bd9dda477832a303c29d017aa4b50bd4ec
pixelzoom <cmalley@pixelzoom.com>
2020-06-09 17:21:32 -0600

    TODOs

--- js/common/model/PopulationModel.js
+++ js/common/model/PopulationModel.js
@@ -185,14 +185,24 @@ class PopulationModel extends PhetioObject {
       }
     };
 
-    // When generations changes, scroll the x-axis.
-    generationsProperty.link( () => scrollToNow() );
+    // When generations changes...
+    generationsProperty.link( () => {
+
+      // scroll the x-axis
+      scrollToNow();
+
+      //TODO update dataProbe if it was previously to the right of data (better place to do this?)
+    } );
 
     // When the sim starts playing, scroll the x-axis.
     isPlayingProperty.link( isPlaying => {
       if ( isPlaying ) {
         scrollToNow();
       }
+    } );
+
+    this.xRangeProperty.link( xRange => {
+      //TODO update dataProbe.generationProperty
     } );
   }
 
