1ed697c6fd45105f8c9267392db266b8a096fc52
pixelzoom <cmalley@pixelzoom.com>
2020-06-19 18:17:15 -0600

    wire up data probe to bunny counts

--- js/common/model/DataProbe.js
+++ js/common/model/DataProbe.js
@@ -19,6 +19,7 @@ import Tandem from '../../../../tandem/js/Tandem.js';
 import NullableIO from '../../../../tandem/js/types/NullableIO.js';
 import NumberIO from '../../../../tandem/js/types/NumberIO.js';
 import naturalSelection from '../../naturalSelection.js';
+import BunnyCounts from './BunnyCounts.js';
 import BunnyCountsIO from './BunnyCountsIO.js';
 import PopulationModel from './PopulationModel.js';
 
@@ -40,6 +41,9 @@ class DataProbe extends PhetioObject {
 
     super( options );
 
+    // @private
+    this.populationModel = populationModel;
+
     // @public
     this.offsetProperty = new Vector2Property( Vector2.ZERO, {
       tandem: options.tandem.createTandem( 'offsetProperty' ),
@@ -55,11 +59,9 @@ class DataProbe extends PhetioObject {
         phetioDocumentation: 'the generation (x-axis) value where the data probe is positioned'
       } );
 
-    // @public dispose is not necessary.
-    this.countsProperty = new DerivedProperty( [ this.generationProperty ],
-
-      //TODO set BunnyCounts based on position of the data probe
-      generation => null, {
+    // @public Set BunnyCounts based on position of the data probe. dispose is not necessary.
+    this.countsProperty = new DerivedProperty( [ this.generationProperty, populationModel.generationsProperty ],
+      ( generation, generations ) => this.getCounts( generation, generations ), {
         tandem: options.tandem.createTandem( 'countsProperty' ),
         phetioType: DerivedPropertyIO( NullableIO( BunnyCountsIO ) ),
         phetioDocumentation: 'counts displayed by the data probe'
@@ -86,6 +88,49 @@ class DataProbe extends PhetioObject {
   dispose() {
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
     super.dispose();
+  }
+
+  //TODO generation and generations is confusing!
+  /**
+   * Gets the bunny counts for a specific generation value.
+   * @param {number} generation
+   * @param {number} generations - current value of the generation clock
+   * @returns {BunnyCounts|null}
+   * @private
+   */
+  getCounts( generation, generations ) {
+    let counts = null;
+    if ( generation <= generations && generations > 0 ) {
+      counts = new BunnyCounts( {
+        totalCount: this.getCount( generation, this.populationModel.totalPoints ),
+        whiteFurCount: this.getCount( generation, this.populationModel.whiteFurPoints ),
+        brownFurCount: this.getCount( generation, this.populationModel.brownFurPoints ),
+        straightEarsCount: this.getCount( generation, this.populationModel.straightEarsPoints ),
+        floppyEarsCount: this.getCount( generation, this.populationModel.floppyEarsPoints ),
+        shortTeethCount: this.getCount( generation, this.populationModel.shortTeethPoints ),
+        longTeethCount: this.getCount( generation, this.populationModel.longTeethPoints )
+      } );
+    }
+    return counts;
+  }
+
+  /**
+   * Gets the population count (y value) for a specific generation (x value).
+   * @param {number} generation
+   * @param {ObservableArray.<Vector2>} points
+   * @returns {number}
+   * @private
+   */
+  getCount( generation, points ) {
+    let count = 0;
+    for ( let i = points.length - 1; i >=0; i-- ) {
+      const point = points.get( i );
+      if ( generation >= point.x ) {
+        count = point.y;
+        break;
+      }
+    }
+    return count;
   }
 }
 
