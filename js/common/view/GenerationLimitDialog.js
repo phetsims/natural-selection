c76e82037400173f9f2ad7009bb6e7d2126ed5a3
pixelzoom <cmalley@pixelzoom.com>
2020-07-02 14:20:00 -0600

    #46

--- js/common/view/GenerationLimitDialog.js
+++ js/common/view/GenerationLimitDialog.js
@@ -8,9 +8,14 @@
 
 import merge from '../../../../phet-core/js/merge.js';
 import OopsDialog from '../../../../scenery-phet/js/OopsDialog.js';
+import Image from '../../../../scenery/js/nodes/Image.js';
+import bunnyBrownFurStraightEarsLongTeethImage from '../../../images/bunny-brownFur-straightEars-longTeeth_png.js';
 import naturalSelection from '../../naturalSelection.js';
 import naturalSelectionStrings from '../../naturalSelectionStrings.js';
 import NaturalSelectionConstants from '../NaturalSelectionConstants.js';
+
+// constants
+const ICON_SCALE = 0.75;
 
 class GenerationLimitDialog extends OopsDialog {
 
@@ -19,10 +24,14 @@ class GenerationLimitDialog extends OopsDialog {
    */
   constructor( options ) {
 
+    const iconNode = new Image( bunnyBrownFurStraightEarsLongTeethImage );
+    iconNode.setScaleMagnitude( -ICON_SCALE, ICON_SCALE );
+
     options = merge( {
       topMargin: 50,
       bottomMargin: 50,
-      scale: NaturalSelectionConstants.DIALOG_SCALE
+      scale: NaturalSelectionConstants.DIALOG_SCALE,
+      iconNode: iconNode
     }, options );
 
     super( naturalSelectionStrings.memoryLimitMessage, options );
