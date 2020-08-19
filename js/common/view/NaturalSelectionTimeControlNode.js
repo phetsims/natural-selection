53e89809ddd0546221f47f350aeb62ad2a619ef1
pixelzoom <cmalley@pixelzoom.com>
2020-08-18 18:16:04 -0600

    use TimeControlNode to see what it looks like, #179

--- js/common/view/NaturalSelectionTimeControlNode.js
+++ js/common/view/NaturalSelectionTimeControlNode.js
@@ -8,68 +8,56 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
 import merge from '../../../../phet-core/js/merge.js';
 import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
-import PlayPauseButton from '../../../../scenery-phet/js/buttons/PlayPauseButton.js';
-import HBox from '../../../../scenery/js/nodes/HBox.js';
-import EnabledNode from '../../../../sun/js/EnabledNode.js';
+import TimeControlNode from '../../../../scenery-phet/js/TimeControlNode.js';
+import TimeSpeed from '../../../../scenery-phet/js/TimeSpeed.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import naturalSelection from '../../naturalSelection.js';
 import NaturalSelectionQueryParameters from '../NaturalSelectionQueryParameters.js';
-import FastForwardButton from './FastForwardButton.js';
 
-class NaturalSelectionTimeControlNode extends HBox {
+class NaturalSelectionTimeControlNode extends TimeControlNode {
 
-  /**
-   * @param {Property.<boolean>} isPlayingProperty
-   * @param {Property.<number>} timeScaleProperty
-   * @param {Object} [options]
-   */
   constructor( isPlayingProperty, timeScaleProperty, options ) {
+
     assert && AssertUtils.assertPropertyOf( isPlayingProperty, 'boolean' );
     assert && AssertUtils.assertPropertyOf( timeScaleProperty, 'number' );
 
     options = merge( {
-      spacing: 10,
+      timeSpeeds: [ TimeSpeed.NORMAL, TimeSpeed.FAST ],
+      buttonGroupXSpacing: 15,
+      playPauseStepButtonOptions: {
+        stepForwardButtonOptions: {
+          //TODO https://github.com/phetsims/natural-selection/issues/179 this is a hack, would need to add TimeControlNode option to omit step button
+          visible: false
+        }
+      },
 
       // phet-io
       tandem: Tandem.REQUIRED
     }, options );
 
-    const playPauseButton = new PlayPauseButton( isPlayingProperty, {
-      radius: 20,
-      listener: () => fastForwardButton.interruptSubtreeInput(),
-      tandem: options.tandem.createTandem( 'playPauseButton' )
+    assert && assert( !options.timeSpeedProperty, 'NaturalSelectionTimeControlNodes sets timeSpeedProperty' );
+    options.timeSpeedProperty = new EnumerationProperty( TimeSpeed, TimeSpeed.NORMAL, {
+      tandem: options.tandem.createTandem( 'timeSpeedProperty' )
     } );
 
-    const fastForwardButton = new FastForwardButton( timeScaleProperty,
-      NaturalSelectionQueryParameters.fastForwardScale, {
-      tandem: options.tandem.createTandem( 'fastForwardButton' )
-    } );
-
-    assert && assert( !options.children, 'NaturalSelectionTimeControl sets children' );
-    options.children = [ playPauseButton, fastForwardButton ];
-
-    super( options );
-    this.initializeEnabledNode( options );
-
-    let isPlayingSaved = isPlayingProperty.value;
-    fastForwardButton.buttonModel.downProperty.link( down => {
-      playPauseButton.enabled = !down;
-      if ( down ) {
-
-        // Disable playPauseButton when fastForwardButton is pressed.
-        playPauseButton.enabled = false;
-        isPlayingSaved = isPlayingProperty.value;
-        isPlayingProperty.value = true;
+    options.timeSpeedProperty.link( timeSpeed => {
+      phet.log && phet.log( `timeSpeed=${timeSpeed}` );
+      if ( timeSpeed === TimeSpeed.NORMAL ) {
+        timeScaleProperty.value = 1;
       }
       else {
-
-        // Restore state of playPauseButton when fastForwardButton is released.
-        playPauseButton.enabled = true;
-        isPlayingProperty.value = isPlayingSaved;
+        timeScaleProperty.value = NaturalSelectionQueryParameters.fastForwardScale;
       }
     } );
+
+    //TODO https://github.com/phetsims/natural-selection/issues/179 this doesn't handle the case where timeScaleProperty
+    // changes and we need to adjust options.timeSpeedProperty.  If we go with this approach, replace model.timeScaleProperty
+    // with timeSpeedProperty. Or maybe do that anyway?
+
+    super( isPlayingProperty, options );
   }
 
   /**
@@ -82,8 +70,72 @@ class NaturalSelectionTimeControlNode extends HBox {
   }
 }
 
-// mix in enabled component into a Node
-EnabledNode.mixInto( NaturalSelectionTimeControlNode );
+// class NaturalSelectionTimeControlNode extends HBox {
+//
+//   /**
+//    * @param {Property.<boolean>} isPlayingProperty
+//    * @param {Property.<number>} timeScaleProperty
+//    * @param {Object} [options]
+//    */
+//   constructor( isPlayingProperty, timeScaleProperty, options ) {
+//     assert && AssertUtils.assertPropertyOf( isPlayingProperty, 'boolean' );
+//     assert && AssertUtils.assertPropertyOf( timeScaleProperty, 'number' );
+//
+//     options = merge( {
+//       spacing: 10,
+//
+//       // phet-io
+//       tandem: Tandem.REQUIRED
+//     }, options );
+//
+//     const playPauseButton = new PlayPauseButton( isPlayingProperty, {
+//       radius: 20,
+//       listener: () => fastForwardButton.interruptSubtreeInput(),
+//       tandem: options.tandem.createTandem( 'playPauseButton' )
+//     } );
+//
+//     const fastForwardButton = new FastForwardButton( timeScaleProperty,
+//       NaturalSelectionQueryParameters.fastForwardScale, {
+//       tandem: options.tandem.createTandem( 'fastForwardButton' )
+//     } );
+//
+//     assert && assert( !options.children, 'NaturalSelectionTimeControl sets children' );
+//     options.children = [ playPauseButton, fastForwardButton ];
+//
+//     super( options );
+//     this.initializeEnabledNode( options );
+//
+//     let isPlayingSaved = isPlayingProperty.value;
+//     fastForwardButton.buttonModel.downProperty.link( down => {
+//       playPauseButton.enabled = !down;
+//       if ( down ) {
+//
+//         // Disable playPauseButton when fastForwardButton is pressed.
+//         playPauseButton.enabled = false;
+//         isPlayingSaved = isPlayingProperty.value;
+//         isPlayingProperty.value = true;
+//       }
+//       else {
+//
+//         // Restore state of playPauseButton when fastForwardButton is released.
+//         playPauseButton.enabled = true;
+//         isPlayingProperty.value = isPlayingSaved;
+//       }
+//     } );
+//   }
+//
+//   /**
+//    * @public
+//    * @override
+//    */
+//   dispose() {
+//     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
+//     super.dispose();
+//   }
+// }
+//
+// // mix in enabled component into a Node
+// EnabledNode.mixInto( NaturalSelectionTimeControlNode );
 
 naturalSelection.register( 'NaturalSelectionTimeControlNode', NaturalSelectionTimeControlNode );
 export default NaturalSelectionTimeControlNode;
