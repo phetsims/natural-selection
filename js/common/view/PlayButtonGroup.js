5b373bf9d8ee548a86194fcb846e6a8627d86e0c
pixelzoom <cmalley@pixelzoom.com>
2020-09-02 16:21:31 -0600

    fix problem with Play button appearing, #217

--- js/common/view/PlayButtonGroup.js
+++ js/common/view/PlayButtonGroup.js
@@ -7,7 +7,6 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import Property from '../../../../axon/js/Property.js';
 import timer from '../../../../axon/js/timer.js';
 import merge from '../../../../phet-core/js/merge.js';
 import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
@@ -76,29 +75,31 @@ class PlayButtonGroup extends Node {
 
     super( options );
 
-    // Make at most 1 button visible. unmultilink is not necessary.
-    Property.multilink(
-      [ simulationModeProperty, bunnyCountProperty ],
-      ( simulationMode, bunnyCount ) => {
+    // Make at most 1 button visible. unlink is not necessary.
+    simulationModeProperty.link( simulationMode => {
+
+        // start with all buttons hidden
+        addAMateButton.visible = false;
+        playButton.visible = false;
+        startOverButton.visible = false;
+
         if ( simulationMode === SimulationMode.STAGED ) {
 
+          // Show 'Add a Mate' or 'Play' button, depending on the size of the population.
           // Make one of these buttons visible on the next frame, so that a double-click on the 'Start Over' button
           // doesn't fire the button that is made visible. See https://github.com/phetsims/natural-selection/issues/166
+          const bunnyCount = bunnyCountProperty.value;
           timer.runOnNextFrame( () => {
             addAMateButton.visible = ( bunnyCount === 1 );
             playButton.visible = ( bunnyCount > 1 );
           } );
-
-          startOverButton.visible = false;
         }
         else if ( simulationMode === SimulationMode.ACTIVE ) {
-          addAMateButton.visible = false;
-          playButton.visible = false;
-          startOverButton.visible = false;
+          // do nothing, all buttons remain hidden
         }
         else if ( simulationMode === SimulationMode.COMPLETED ) {
-          addAMateButton.visible = false;
-          playButton.visible = false;
+
+          // Show 'Start Over' button.
           startOverButton.visible = true;
         }
         else {
