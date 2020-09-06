5a09377d000076285c2733edf59619c77a426828
pixelzoom <cmalley@pixelzoom.com>
2020-09-05 18:48:05 -0600

    add options.documentation to setIOTypeFields, https://github.com/phetsims/tandem/issues/188

--- js/common/model/setIOTypeFields.js
+++ js/common/model/setIOTypeFields.js
@@ -6,21 +6,32 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import merge from '../../../../phet-core/js/merge.js';
 import ObjectIO from '../../../../tandem/js/types/ObjectIO.js';
 import naturalSelection from '../../naturalSelection.js';
 
 // constants
 const IO_TYPE_SUFFIX = 'IO';
 
-function setIOTypeFields( IOType, ioTypeName, coreType ) {
+/**
+ * @param {function} ioType - an IO Type
+ * @param ioTypeName - classname of IOType
+ * @param coreType - the corresponding Core Type
+ * @param {Object} [options]
+ */
+function setIOTypeFields( ioType, ioTypeName, coreType, options ) {
+
+  options = merge( {
+    documentation: null // {string} if not provided, default is defined below
+  }, options );
 
   // Fill in static fields in the IO Type.
-  IOType.typeName = ioTypeName;
-  IOType.documentation = `IO Type for ${ioTypeName.substring( 0, ioTypeName.length - IO_TYPE_SUFFIX.length )}`;
-  IOType.validator = { valueType: coreType };
+  ioType.typeName = ioTypeName;
+  ioType.documentation = options.documentation || `IO Type for ${ioTypeName.substring( 0, ioTypeName.length - IO_TYPE_SUFFIX.length )}`;
+  ioType.validator = { valueType: coreType };
 
   // Verify that we've defined a valid IO Type.
-  ObjectIO.validateSubtype( IOType );
+  ObjectIO.validateSubtype( ioType );
 }
 
 naturalSelection.register( 'setIOTypeFields', setIOTypeFields );
