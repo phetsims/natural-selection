1d5e633aa58c69eb3427b0ce1f625f8ae33eb1b3
pixelzoom <cmalley@pixelzoom.com>
2020-09-28 10:46:36 -0600

    add Liam to credits, #2

--- js/natural-selection-main.js
+++ js/natural-selection-main.js
@@ -31,7 +31,7 @@ simLauncher.launch( () => {
       leadDesign: 'Amanda McGarry',
       softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
       team: 'Wendy Adams, Megan Hoffman, Oliver Nix, Jonathan Olson, Ariel Paul, Kathy Perkins, Noah Podolefsky, Carl Wieman',
-      qualityAssurance: 'Logan Bray, Steele Dalton, Katie Woessner',
+      qualityAssurance: 'Logan Bray, Steele Dalton, Liam Mulhall, Katie Woessner',
       graphicArts: 'Megan Lai'
     }
   } );
