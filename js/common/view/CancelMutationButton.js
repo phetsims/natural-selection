07eeff7c2c59754344369154e0123593cb44c2bb
phet-dev <phetadmin@gmail.com>
2021-07-10 03:36:03 -0600

    update copyright dates from daily grunt work

--- js/common/view/CancelMutationButton.js
+++ js/common/view/CancelMutationButton.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * CancelMutationButton is the button that appears in the 'Mutation Coming' alert, used to cancel a mutation.
--- js/common/view/MutationIconNode.js
+++ js/common/view/MutationIconNode.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * MutationIconNode is the mutation icon that appears in the Pedigree tree and 'Add Mutations' panel.
