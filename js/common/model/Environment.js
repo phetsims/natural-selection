9656c7cd98802ed4aca1b2cdaf142eba3704f4d7
phet-dev <phetadmin@gmail.com>
2022-01-07 03:24:57 -0700

    update copyright dates from daily grunt work

--- js/common/model/Environment.js
+++ js/common/model/Environment.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * Environment is an enumeration of the environments where the bunnies may live.
--- js/common/model/XDirection.js
+++ js/common/model/XDirection.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * XDirection is the direction that an Organism (bunny, wolf, shrub) is facing along the x axis.
