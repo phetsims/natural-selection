5f949725e6c8e69412352b81057744cbfc96c4fb
phet-dev <phetadmin@gmail.com>
2022-01-12 03:26:46 -0700

    update copyright dates from daily grunt work

--- js/common/model/WolfCollection.js
+++ js/common/model/WolfCollection.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * WolfCollection is the collection of Wolf instances, with methods for managing that collection.
