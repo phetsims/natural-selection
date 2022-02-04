988a9681b0180767fedd8b800817c7c003d551fa
phet-dev <phetadmin@gmail.com>
2022-02-04 03:22:28 -0700

    update copyright dates from daily grunt work

--- js/common/view/environment/OrganismSpriteInstance.js
+++ js/common/view/environment/OrganismSpriteInstance.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * OrganismSpriteInstance is a specialization of SpriteInstance for organisms (bunnies, wolves, shrubs).
