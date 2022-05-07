4bdd1f969c50c3ae69b0feaae3490a478a4a2757
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:43 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/naturalSelectionStrings.ts
+++ js/naturalSelectionStrings.ts
@@ -9,64 +9,64 @@ import naturalSelection from './naturalSelection.js';
 
 type StringsType = {
   'natural-selection': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'intro': string,
-    'lab': string
-  },
-  'addAMate': string,
-  'population': string,
-  'proportions': string,
-  'pedigree': string,
-  'total': string,
-  'dataProbe': string,
-  'whiteFur': string,
-  'brownFur': string,
-  'straightEars': string,
-  'floppyEars': string,
-  'shortTeeth': string,
-  'longTeeth': string,
-  'values': string,
-  'fur': string,
-  'ears': string,
-  'teeth': string,
-  'environmentalFactors': string,
-  'environmentalFactor': string,
-  'wolves': string,
-  'toughFood': string,
-  'limitedFood': string,
-  'addMutations': string,
-  'addMutation': string,
-  'alleles': string,
-  'dominant': string,
-  'recessive': string,
-  'mutationComing': string,
-  'furDominant': string,
-  'furRecessive': string,
-  'earsDominant': string,
-  'earsRecessive': string,
-  'teethDominant': string,
-  'teethRecessive': string,
-  'generation': string,
-  'generationValue': string,
-  'startOfGeneration': string,
-  'endOfGeneration': string,
-  'currently': string,
-  'countBunnies': string,
-  'oneBunny': string,
-  'play': string,
-  'startOver': string,
-  'bunniesHaveTakenOverTheWorld': string,
-  'allOfTheBunniesHaveDied': string,
-  'valuePercent': string,
-  'lessThanValuePercent': string,
-  'greaterThanValuePercent': string,
-  'selectABunny': string,
-  'none': string,
-  'noData': string,
-  'zoomOutToSeeData': string,
-  'memoryLimitMessage': string
+    'intro': string;
+    'lab': string;
+  };
+  'addAMate': string;
+  'population': string;
+  'proportions': string;
+  'pedigree': string;
+  'total': string;
+  'dataProbe': string;
+  'whiteFur': string;
+  'brownFur': string;
+  'straightEars': string;
+  'floppyEars': string;
+  'shortTeeth': string;
+  'longTeeth': string;
+  'values': string;
+  'fur': string;
+  'ears': string;
+  'teeth': string;
+  'environmentalFactors': string;
+  'environmentalFactor': string;
+  'wolves': string;
+  'toughFood': string;
+  'limitedFood': string;
+  'addMutations': string;
+  'addMutation': string;
+  'alleles': string;
+  'dominant': string;
+  'recessive': string;
+  'mutationComing': string;
+  'furDominant': string;
+  'furRecessive': string;
+  'earsDominant': string;
+  'earsRecessive': string;
+  'teethDominant': string;
+  'teethRecessive': string;
+  'generation': string;
+  'generationValue': string;
+  'startOfGeneration': string;
+  'endOfGeneration': string;
+  'currently': string;
+  'countBunnies': string;
+  'oneBunny': string;
+  'play': string;
+  'startOver': string;
+  'bunniesHaveTakenOverTheWorld': string;
+  'allOfTheBunniesHaveDied': string;
+  'valuePercent': string;
+  'lessThanValuePercent': string;
+  'greaterThanValuePercent': string;
+  'selectABunny': string;
+  'none': string;
+  'noData': string;
+  'zoomOutToSeeData': string;
+  'memoryLimitMessage': string;
 };
 
 const naturalSelectionStrings = getStringModule( 'NATURAL_SELECTION' ) as StringsType;
