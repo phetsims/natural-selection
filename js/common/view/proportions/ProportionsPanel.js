ab299e1862288515e8cb742ebdd379d120ff846f
phet-dev <phetadmin@gmail.com>
2022-06-24 03:57:22 -0600

    update copyright dates from daily grunt work

--- js/common/view/proportions/ProportionsPanel.js
+++ js/common/view/proportions/ProportionsPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * ProportionsPanel is the panel that contains controls for the 'Proportions' graph.
