dec4d84f7c8d9a0ec226ae0b2b496d3e76830887
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-11 16:26:47 -0600

    Sim screenProperty => selectedScreenProperty and other screen changes, see https://github.com/phetsims/joist/issues/827

--- js/natural-selection-main.js
+++ js/natural-selection-main.js
@@ -45,7 +45,7 @@ simLauncher.launch( () => {
   // Log the name of the active screen, to make the console logging easier to grok.
   // unlink is not necessary.
   if ( phet.log ) {
-    sim.screenProperty.link( screen =>
+    sim.selectedScreenProperty.link( screen =>
       phet.log && phet.log( `>>>>>> ${screen.nameProperty.value} screen is active` )
     );
   }
