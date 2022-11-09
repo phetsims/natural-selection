6b5ccefa1c01b13ecb1d247ea41f63cc38380b81
phet-dev <phetadmin@gmail.com>
2022-11-09 03:51:27 -0700

    update copyright dates from daily grunt work

--- js/common/view/OriginNode.ts
+++ js/common/view/OriginNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * OriginNode is a debugging node used to show an object's origin. Enable via ?showOrigin.
--- js/lab/view/LabScreenView.ts
+++ js/lab/view/LabScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * LabScreenView is the view for the 'Lab' screen. Adds no additional functionality to NaturalSelectionScreenView,
