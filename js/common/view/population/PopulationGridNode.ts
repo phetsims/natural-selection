394aa05da00be84b7616c6ed8965c4bf7e2d1635
Chris Malley <cmalley@pixelzoom.com>
2022-11-10 12:52:28 -0700

    convert to TypeScript

--- js/common/view/population/PopulationGridNode.ts
+++ js/common/view/population/PopulationGridNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * PopulationGridNode is the 2D grid for the Population graph, including grid lines and tick marks.
  * The grid scrolls horizontally as the x-axis range changes, and zooms in/out vertically as the y-axis range changes.
@@ -8,12 +7,14 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import TReadOnlyProperty from '../../../../../axon/js/TReadOnlyProperty.js';
 import Range from '../../../../../dot/js/Range.js';
 import { Shape } from '../../../../../kite/js/imports.js';
-import merge from '../../../../../phet-core/js/merge.js';
-import AssertUtils from '../../../../../phetcommon/js/AssertUtils.js';
+import optionize from '../../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../../phet-core/js/types/PickRequired.js';
+import StrictOmit from '../../../../../phet-core/js/types/StrictOmit.js';
 import PhetFont from '../../../../../scenery-phet/js/PhetFont.js';
-import { Node, Path, Rectangle, Text } from '../../../../../scenery/js/imports.js';
+import { Node, NodeOptions, NodeTranslationOptions, Path, PathOptions, Rectangle, Text } from '../../../../../scenery/js/imports.js';
 import naturalSelection from '../../../naturalSelection.js';
 import PopulationModel from '../../model/PopulationModel.js';
 import NaturalSelectionColors from '../../NaturalSelectionColors.js';
@@ -25,29 +26,32 @@ const TICK_MARKS_LENGTH = 4;
 const TICK_LABEL_SPACING = 3;
 const TICK_MARKS_FONT = new PhetFont( 10 );
 
-class PopulationGridNode extends Node {
+type SelfOptions = {
 
-  /**
-   * @param {PopulationModel} populationModel
-   * @param {Object} [options]
-   */
-  constructor( populationModel, options ) {
+  // dimensions of the grid (sans tick marks) in view coordinates
+  gridWidth?: number;
+  gridHeight?: number;
+};
 
-    assert && assert( populationModel instanceof PopulationModel, 'invalid populationModel' );
+type PopulationGridNodeOptions = SelfOptions & NodeTranslationOptions;
 
-    options = merge( {
+export default class PopulationGridNode extends Node {
 
-      // dimensions of the grid (sans tick marks) in view coordinates
+  public constructor( populationModel: PopulationModel, providedOptions?: PopulationGridNodeOptions ) {
+
+    const options = optionize<PopulationGridNodeOptions, SelfOptions, NodeOptions>()( {
+
+      // SelfOptions
       gridWidth: 100,
       gridHeight: 100
-    }, options );
+    }, providedOptions );
 
     // Background rectangle
     const rectangleNode = new Rectangle( 0, 0, options.gridWidth, options.gridHeight, {
       fill: NaturalSelectionColors.POPULATION_GRAPH_FILL
     } );
 
-    // Grid lines for the x axis
+    // Grid lines for the x-axis
     const xGridLines = new VerticalLines( populationModel.xRangeProperty, {
       xAxisWidth: options.gridWidth,
       xSpacingModel: populationModel.xAxisTickSpacing,
@@ -57,11 +61,11 @@ class PopulationGridNode extends Node {
         lineWidth: GRID_LINES_LINE_WIDTH
       },
 
-      // Clip to the background bounds, because we'll be horizontally translating the x grid lines
+      // Clip to the background bounds, because we'll be horizontally translating the x-axis grid lines
       clipArea: Shape.rectangle( 0, 0, options.gridWidth, options.gridHeight )
     } );
 
-    // Grid lines for the y axis
+    // Grid lines for the y-axis
     const yGridLines = new HorizontalLines( populationModel.yRangeProperty, () => populationModel.getYTickSpacing(), {
       yAxisHeight: options.gridHeight,
       lineLength: options.gridWidth,
@@ -74,7 +78,7 @@ class PopulationGridNode extends Node {
       children: [ xGridLines, yGridLines ]
     } );
 
-    // Tick-mark lines for the x axis
+    // Tick-mark lines for the x-axis
     const xTickLines = new VerticalLines( populationModel.xRangeProperty, {
       xSpacingModel: populationModel.xAxisTickSpacing,
       xAxisWidth: options.gridWidth,
@@ -85,18 +89,18 @@ class PopulationGridNode extends Node {
       },
       top: rectangleNode.bottom,
 
-      // Clip to the tick mark bounds below the x axis, because we'll be horizontally translating the x tick marks
+      // Clip to the tick mark bounds below the x-axis, because we'll be horizontally translating the x-axis tick marks
       clipArea: Shape.rectangle( 0, 0, options.gridWidth, options.gridHeight + TICK_MARKS_LENGTH )
     } );
 
-    // Tick-mark labels for the x axis
+    // Tick-mark labels for the x-axis
     const xTickLabels = new XTickLabels( populationModel.xRangeProperty, {
       xSpacingModel: populationModel.xAxisTickSpacing,
       xAxisWidth: options.gridWidth,
       top: xTickLines.bottom + TICK_LABEL_SPACING
     } );
 
-    // Tick-mark lines for the y axis
+    // Tick-mark lines for the y-axis
     const yTickLines = new HorizontalLines( populationModel.yRangeProperty, () => populationModel.getYTickSpacing(), {
       yAxisHeight: options.gridHeight,
       lineLength: TICK_MARKS_LENGTH,
@@ -105,7 +109,7 @@ class PopulationGridNode extends Node {
       right: rectangleNode.left
     } );
 
-    // Tick-mark labels for the y axis
+    // Tick-mark labels for the y-axis
     const yTickLabels = new YTickLabels( populationModel.yRangeProperty, () => populationModel.getYTickSpacing(), {
       yAxisHeight: options.gridHeight,
       right: yTickLines.left - TICK_LABEL_SPACING
@@ -122,17 +126,12 @@ class PopulationGridNode extends Node {
       lineWidth: 1.5
     } );
 
-    assert && assert( !options.children, 'PopulationGraphBackgroundNode sets children' );
     options.children = [ rectangleNode, gridLinesNode, tickMarksNode, frameNode ];
 
     super( options );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
     super.dispose();
   }
@@ -143,22 +142,32 @@ class PopulationGridNode extends Node {
  * changes as time progresses.  So we create a single Shape for the vertical lines, then translate it as the
  * x-axis range changes.  Bounds are clipped to the background dimensions.
  */
+
+type VerticalLinesSelfOptions = {
+  xSpacingModel?: number; // spacing between lines, in model coordinates
+  xAxisWidth?: number; // width of the x-axis, in view coordinates
+  lineLength?: number; // vertical length of the lines, in view coordinates
+  pathOptions?: PathOptions; // options passed to the Path subcomponent, which draws the lines
+};
+
+type VerticalLinesOptions = VerticalLinesSelfOptions & NodeTranslationOptions &
+  PickRequired<NodeOptions, 'clipArea'>;
+
 class VerticalLines extends Node {
 
   /**
-   * @param {Property.<Range>} xRangeProperty - range of the x-axis, in model coordinates
-   * @param {Object} [options]
+   * @param xRangeProperty - range of the x-axis, in model coordinates
+   * @param [providedOptions]
    */
-  constructor( xRangeProperty, options ) {
+  public constructor( xRangeProperty: TReadOnlyProperty<Range>, providedOptions?: VerticalLinesOptions ) {
 
-    assert && AssertUtils.assertPropertyOf( xRangeProperty, Range );
+    const options = optionize<VerticalLinesOptions, StrictOmit<VerticalLinesSelfOptions, 'pathOptions'>, NodeOptions>()( {
 
-    options = merge( {
-      xSpacingModel: 1, // spacing between lines, in model coordinates
-      xAxisWidth: 100, // width of the x axis, in view coordinates
-      lineLength: 100, // vertical length of the lines, in view coordinates
-      pathOptions: null // options passed to the Path subcomponent, which draws the lines
-    }, options );
+      // VerticalLinesSelfOptions
+      xSpacingModel: 1,
+      xAxisWidth: 100,
+      lineLength: 100
+    }, providedOptions );
 
     // Compute the number of lines and their spacing, in view coordinates
     const numberOfLines = getNumberOfVerticalLines( xRangeProperty.value, options.xSpacingModel );
@@ -174,7 +183,6 @@ class VerticalLines extends Node {
     const path = new Path( shape, options.pathOptions );
 
     // Wrapped in a Node because we're going to translate the Path
-    assert && assert( !options.children, 'VerticalLines sets children' );
     options.children = [ path ];
 
     // Translate the lines as time progresses. unlink is not necessary.
@@ -185,11 +193,7 @@ class VerticalLines extends Node {
     super( options );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'VerticalLines does not support dispose' );
     super.dispose();
   }
@@ -199,22 +203,32 @@ class VerticalLines extends Node {
  * HorizontalLines are used for y-axis tick marks and grid lines.
  * They are recreated on demand, when the zoom control is used.
  */
+
+type HorizontalLinesSelfOptions = {
+  yAxisHeight?: number; // y-axis height, in view coordinates
+  lineLength?: number; // line length, in view coordinates
+};
+
+type HorizontalLinesOptions = HorizontalLinesSelfOptions & NodeTranslationOptions &
+  PickRequired<PathOptions, 'stroke' | 'lineWidth'>;
+
 class HorizontalLines extends Path {
 
   /**
-   * @param {ReadOnlyProperty.<Range>} yRangeProperty - range of the y-axis range, in model coordinates
-   * @param {function:number} getYSpacing - gets the y-spacing for the current value of yRangeProperty
-   * @param {Object} [options]
+   * @param yRangeProperty - range of the y-axis range, in model coordinates
+   * @param getYSpacing - gets the y-spacing for the current value of yRangeProperty
+   * @param [providedOptions]
    */
-  constructor( yRangeProperty, getYSpacing, options ) {
+  public constructor( yRangeProperty: TReadOnlyProperty<Range>,
+                      getYSpacing: () => number,
+                      providedOptions?: HorizontalLinesOptions ) {
 
-    assert && AssertUtils.assertAbstractPropertyOf( yRangeProperty, Range );
-    assert && assert( typeof getYSpacing === 'function', 'invalid getYSpacing' );
+    const options = optionize<HorizontalLinesOptions, HorizontalLinesSelfOptions, PathOptions>()( {
 
-    options = merge( {
-      yAxisHeight: 100, // y axis height, in view coordinates
-      lineLength: 100 // line length, in view coordinates
-    }, options );
+      // HorizontalLinesSelfOptions
+      yAxisHeight: 100,
+      lineLength: 100
+    }, providedOptions );
 
     super( new Shape() );
 
@@ -239,11 +253,7 @@ class HorizontalLines extends Path {
     this.mutate( options );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'HorizontalLines does not support dispose' );
     super.dispose();
   }
@@ -253,20 +263,28 @@ class HorizontalLines extends Path {
  * XTickLabels renders the x-axis tick-mark labels. (The tick-mark lines are rendered by VerticalLines.)
  * There is a static number of labels, reused and repositioned as the x-axis range changes.
  */
+
+type XTickLabelsSelfOptions = {
+  xSpacingModel?: number; // spacing between lines, in model coordinates
+  xAxisWidth?: number; // x-axis width, in view coordinates
+};
+
+type XTickLabelsOptions = XTickLabelsSelfOptions & NodeTranslationOptions;
+
 class XTickLabels extends Node {
 
   /**
-   * @param {Property.<Range>} xRangeProperty - the x-axis range, in model coordinates
-   * @param {Object} [options]
+   * @param xRangeProperty - the x-axis range, in model coordinates
+   * @param [providedOptions]
    */
-  constructor( xRangeProperty, options ) {
+  public constructor( xRangeProperty: TReadOnlyProperty<Range>, providedOptions?: XTickLabelsOptions ) {
 
-    assert && AssertUtils.assertPropertyOf( xRangeProperty, Range );
+    const options = optionize<XTickLabelsOptions, XTickLabelsSelfOptions, NodeOptions>()( {
 
-    options = merge( {
-      xSpacingModel: 1, // spacing between lines, in model coordinates
-      xAxisWidth: 100 // x axis width, in view coordinates
-    }, options );
+      // XTickLabelsSelfOptions
+      xSpacingModel: 1,
+      xAxisWidth: 100
+    }, providedOptions );
 
     // Compute the number of labels and their spacing, in view coordinates
     const numberOfLabels = getNumberOfVerticalLines( xRangeProperty.value, options.xSpacingModel );
@@ -275,14 +293,13 @@ class XTickLabels extends Node {
     // Create a fixed number of labels.
     // These instances of Text do not take a string Property because they display a number.
     // The numbers and positions are adjusted by xRangeProperty listener below.
-    const labelNodes = []; // {Text[]}
+    const labelNodes: Text[] = [];
     for ( let i = 0; i < numberOfLabels; i++ ) {
       labelNodes.push( new Text( i, {
         font: TICK_MARKS_FONT
       } ) );
     }
 
-    assert && assert( !options.children, 'XTickLabels set children' );
     options.children = labelNodes;
 
     super( options );
@@ -305,11 +322,7 @@ class XTickLabels extends Node {
     } );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'XTickLabels does not support dispose' );
     super.dispose();
   }
@@ -319,23 +332,29 @@ class XTickLabels extends Node {
  * YTickLabels renders the y-axis tick-mark labels. (The tick-mark lines are rendered by HorizontalLines.)
  * They are recreated on demand, when the zoom control is used.
  */
+
+type YTickLabelsSelfOptions = {
+  yAxisHeight?: number; // y-axis height, in view coordinates
+};
+
+type YTickLabelsOptions = YTickLabelsSelfOptions & NodeTranslationOptions;
+
 class YTickLabels extends Node {
 
   /**
-   * @param {ReadOnlyProperty.<Range>} yRangeProperty - the y-axis range, in model coordinates
-   * @param {function:number} getYSpacing - gets the y-spacing for the current value of yRangeProperty
-   * @param {Object} [options]
+   * @param yRangeProperty - the y-axis range, in model coordinates
+   * @param getYSpacing - gets the y-spacing for the current value of yRangeProperty
+   * @param [providedOptions]
    */
-  constructor( yRangeProperty, getYSpacing, options ) {
+  public constructor( yRangeProperty: TReadOnlyProperty<Range>,
+                      getYSpacing: () => number,
+                      providedOptions?: YTickLabelsOptions ) {
 
-    assert && AssertUtils.assertAbstractPropertyOf( yRangeProperty, Range );
-    assert && assert( typeof getYSpacing === 'function', 'invalid getYSpacing' );
+    const options = optionize<YTickLabelsOptions, YTickLabelsSelfOptions, NodeOptions>()( {
 
-    options = merge( {
-      yAxisHeight: 100 // y axis height, in view coordinates
-    }, options );
-
-    assert && assert( !options.children, 'YTickLabels sets children' );
+      // YTickLabelsSelfOptions
+      yAxisHeight: 100
+    }, providedOptions );
 
     super();
 
@@ -366,11 +385,7 @@ class YTickLabels extends Node {
     this.mutate( options );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'YTickLabels does not support dispose' );
     super.dispose();
   }
@@ -378,45 +393,40 @@ class YTickLabels extends Node {
 
 /**
  * Gets the number of vertical lines in the grid.
- * @param {Range} xRange - range of the x axis, in model coordinates
- * @param {number} xSpacing - space between vertical grid lines, in model coordinates
- * @returns {number}
+ * @param xRange - range of the x-axis, in model coordinates
+ * @param xSpacing - space between vertical grid lines, in model coordinates
  */
-function getNumberOfVerticalLines( xRange, xSpacing ) {
+function getNumberOfVerticalLines( xRange: Range, xSpacing: number ): number {
   return Math.floor( xRange.getLength() / xSpacing ) + 1;
 }
 
 /**
  * Gets the spacing between vertical lines, in view coordinates.
- * @param {Range} xRange - range of the x axis, in model coordinates
- * @param {number} xSpacing - space between vertical grid lines, in model coordinates
- * @param {number} xAxisWidth - width of the x axis, in view coordinates
- * @returns {number}
+ * @param xRange - range of the x-axis, in model coordinates
+ * @param xSpacing - space between vertical grid lines, in model coordinates
+ * @param xAxisWidth - width of the x-axis, in view coordinates
  */
-function getXSpacingView( xRange, xSpacing, xAxisWidth ) {
+function getXSpacingView( xRange: Range, xSpacing: number, xAxisWidth: number ): number {
   return ( xSpacing / xRange.getLength() ) * xAxisWidth;
 }
 
 /**
  * Gets the number of horizontal lines in the grid.
- * @param {Range} yRange - range of the y axis, in model coordinates
- * @param {number} ySpacing - space between horizontal grid lines, in model coordinates
- * @returns {number}
+ * @param yRange - range of the y-axis, in model coordinates
+ * @param ySpacing - space between horizontal grid lines, in model coordinates
  */
-function getNumberOfHorizontalLines( yRange, ySpacing ) {
+function getNumberOfHorizontalLines( yRange: Range, ySpacing: number ): number {
   return Math.floor( yRange.getLength() / ySpacing ) + 1;
 }
 
 /**
  * Gets the spacing between horizontal lines, in view coordinates.
- * @param {Range} yRange - range of the y axis, in model coordinates
- * @param {number} ySpacing - space between horizontal grid lines, in model coordinates
- * @param {number} yAxisHeight - width of the y axis, in view coordinates
- * @returns {number}
+ * @param yRange - range of the y-axis, in model coordinates
+ * @param ySpacing - space between horizontal grid lines, in model coordinates
+ * @param yAxisHeight - width of the y-axis, in view coordinates
  */
-function getYSpacingView( yRange, ySpacing, yAxisHeight ) {
+function getYSpacingView( yRange: Range, ySpacing: number, yAxisHeight: number ): number {
   return ( ySpacing / yRange.getLength() ) * yAxisHeight;
 }
 
 naturalSelection.register( 'PopulationGridNode', PopulationGridNode );
-export default PopulationGridNode;
