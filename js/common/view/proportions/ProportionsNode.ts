9c1be3c10ac957c0169472a6ae09334e26873887
Chris Malley <cmalley@pixelzoom.com>
2022-11-10 15:36:02 -0700

    convert to TypeScript

--- js/common/view/proportions/ProportionsNode.ts
+++ js/common/view/proportions/ProportionsNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2019-2021, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * ProportionsNode is the parent for all parts of the Proportions view.
  *
@@ -8,42 +7,37 @@
  */
 
 import Dimension2 from '../../../../../dot/js/Dimension2.js';
-import merge from '../../../../../phet-core/js/merge.js';
-import { HBox } from '../../../../../scenery/js/imports.js';
-import Tandem from '../../../../../tandem/js/Tandem.js';
+import optionize, { EmptySelfOptions } from '../../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../../phet-core/js/types/PickRequired.js';
+import { HBox, HBoxOptions, NodeTranslationOptions } from '../../../../../scenery/js/imports.js';
 import naturalSelection from '../../../naturalSelection.js';
+import Gene from '../../model/Gene.js';
 import GenePool from '../../model/GenePool.js';
 import ProportionsModel from '../../model/ProportionsModel.js';
 import NaturalSelectionConstants from '../../NaturalSelectionConstants.js';
 import ProportionsGraphNode from './ProportionsGraphNode.js';
 import ProportionsPanel from './ProportionsPanel.js';
 
+type SelfOptions = EmptySelfOptions;
+
+type ProportionsNodeOptions = SelfOptions & NodeTranslationOptions & PickRequired<HBoxOptions, 'tandem'>;
+
 export default class ProportionsNode extends HBox {
 
-  /**
-   * @param {ProportionsModel} proportionsModel
-   * @param {GenePool} genePool
-   * @param {Dimension2} size - dimensions of the rectangle available for this Node and its children
-   * @param {Object} [options]
-   */
-  constructor( proportionsModel, genePool, size, options ) {
+  private readonly proportionsPanel: ProportionsPanel;
+  private readonly proportionsGraphNode: ProportionsGraphNode;
 
-    assert && assert( proportionsModel instanceof ProportionsModel, 'invalid proportionsModel' );
-    assert && assert( genePool instanceof GenePool, 'invalid genePool' );
-    assert && assert( size instanceof Dimension2, 'invalid size' );
+  public constructor( proportionsModel: ProportionsModel, genePool: GenePool, size: Dimension2, providedOptions: ProportionsNodeOptions ) {
 
-    options = merge( {
+    const options = optionize<ProportionsNodeOptions, SelfOptions, HBoxOptions>()( {
 
-      // HBox options
+      // HBoxOptions
       spacing: NaturalSelectionConstants.SCREEN_VIEW_X_SPACING,
       align: 'center',
       excludeInvisibleChildrenFromBounds: false,
-
-      // phet-io
-      tandem: Tandem.REQUIRED,
       phetioDocumentation: 'the Proportions graph and its control panel',
       visiblePropertyOptions: { phetioReadOnly: true }
-    }, options );
+    }, providedOptions );
 
     // Divy up the width
     const panelWidth = 0.2 * size.width;
@@ -61,7 +55,6 @@ export default class ProportionsNode extends HBox {
       tandem: options.tandem.createTandem( 'proportionsGraphNode' )
     } );
 
-    assert && assert( !options.children, 'ProportionsNode sets children' );
     options.children = [ proportionsPanel, proportionsGraphNode ];
 
     super( options );
@@ -71,27 +64,19 @@ export default class ProportionsNode extends HBox {
       tandem: options.tandem.createTandem( 'proportionsModel' )
     } );
 
-    // @private
-    this.proportionsPanel = proportionsPanel; // {ProportionsPanel}
-    this.proportionsGraphNode = proportionsGraphNode; // {ProportionsGraphNode}
+    this.proportionsPanel = proportionsPanel;
+    this.proportionsGraphNode = proportionsGraphNode;
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
     super.dispose();
   }
 
   /**
    * Sets visibility of the UI components related to a specific gene.
-   * @param {Gene} gene
-   * @param {boolean} visible
-   * @public
    */
-  setGeneVisible( gene, visible ) {
+  public setGeneVisible( gene: Gene, visible: boolean ): void {
     this.proportionsPanel.setGeneVisible( gene, visible );
     this.proportionsGraphNode.setGeneVisible( gene, visible );
   }
