bce3f7217975b01a605512099cdf9507bfe8d1fd
pixelzoom <cmalley@pixelzoom.com>
2022-11-10 00:12:59 -0700

    convert to TypeScript

--- js/common/view/GenesVisibilityManager.ts
+++ js/common/view/GenesVisibilityManager.ts
@@ -1,6 +1,5 @@
 // Copyright 2020-2021, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * GenesVisibilityManager manages the visibility of UI components that are related to specific genes.
  * This is a PhET-iO only feature, available in Studio. Via a set of Properties, all UI components related to
@@ -10,54 +9,55 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
-import merge from '../../../../phet-core/js/merge.js';
+import BooleanProperty, { BooleanPropertyOptions } from '../../../../axon/js/BooleanProperty.js';
+import Property from '../../../../axon/js/Property.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
-import Tandem from '../../../../tandem/js/Tandem.js';
+import { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
 import naturalSelection from '../../naturalSelection.js';
+import Gene from '../model/Gene.js';
 import GenePool from '../model/GenePool.js';
 import AddMutationsPanel from './AddMutationsPanel.js';
 import PedigreeNode from './pedigree/PedigreeNode.js';
 import PopulationNode from './population/PopulationNode.js';
 import ProportionsNode from './proportions/ProportionsNode.js';
 
-class GenesVisibilityManager {
+type SelfOptions = {
 
-  /**
-   * @param {GenePool} genePool
-   * @param {AddMutationsPanel} addMutationsPanel
-   * @param {PopulationNode} populationNode
-   * @param {ProportionsNode} proportionsNode
-   * @param {PedigreeNode} pedigreeNode
-   * @param {Object} [options]
-   */
-  constructor( genePool, addMutationsPanel, populationNode, proportionsNode, pedigreeNode, options ) {
+  // whether the user-interface for these features is visible
+  furVisible?: boolean;
+  earsVisible?: boolean;
+  teethVisible?: boolean;
+};
 
-    assert && assert( genePool instanceof GenePool, 'invalid genePool' );
-    assert && assert( addMutationsPanel instanceof AddMutationsPanel, 'invalid addMutationsPanel' );
-    assert && assert( populationNode instanceof PopulationNode, 'invalid populationNode' );
-    assert && assert( proportionsNode instanceof ProportionsNode, 'invalid proportionsNode' );
-    assert && assert( pedigreeNode instanceof PedigreeNode, 'invalid pedigreeNode' );
+type GenesVisibilityManagerOptions = SelfOptions & PickRequired<PhetioObjectOptions, 'tandem'>;
 
-    options = merge( {
+export default class GenesVisibilityManager {
 
-      // whether the user-interface for these features is visible
+  // Properties used to configure the UI. For use in Studio only, and exist for the lifetime of the sim.
+  private readonly furVisibleProperty: Property<boolean>;
+  private readonly earsVisibleProperty: Property<boolean>;
+  private readonly teethVisibleProperty: Property<boolean>;
+
+  public constructor( genePool: GenePool, addMutationsPanel: AddMutationsPanel, populationNode: PopulationNode,
+                      proportionsNode: ProportionsNode, pedigreeNode: PedigreeNode,
+                      providedOptions: GenesVisibilityManagerOptions ) {
+
+    const options = optionize<GenesVisibilityManagerOptions, SelfOptions>()( {
+
+      // SelfOptions
       furVisible: true,
       earsVisible: true,
-      teethVisible: true,
-
-      // phet-io
-      tandem: Tandem.REQUIRED
-    }, options );
+      teethVisible: true
+    }, providedOptions );
 
     /**
      * Creates a Property that controls the visibility of all UI components related to a gene.
-     * @param {Gene} gene
-     * @param {boolean} visible
-     * @param {Object} [options] - options to BooleanProperty
-     * @returns {BooleanProperty}
      */
-    function createGeneVisibleProperty( gene, visible, options ) {
+    function createGeneVisibleProperty(
+      gene: Gene, visible: boolean,
+      options: PickRequired<BooleanPropertyOptions, 'tandem' | 'phetioDocumentation'> ): Property<boolean> {
 
       const property = new BooleanProperty( visible, options );
 
@@ -74,28 +74,25 @@ class GenesVisibilityManager {
 
     const template = 'sets the visibility of all user-interface components related to {{name}} for this screen';
 
-    // @private Properties used to configure the UI. For use in Studio only, and exist for the lifetime of the sim.
     this.furVisibleProperty = createGeneVisibleProperty( genePool.furGene, options.furVisible, {
       tandem: options.tandem.createTandem( 'furVisibleProperty' ),
       phetioDocumentation: StringUtils.fillIn( template, { name: 'Fur' } )
     } );
+
     this.earsVisibleProperty = createGeneVisibleProperty( genePool.earsGene, options.earsVisible, {
       tandem: options.tandem.createTandem( 'earsVisibleProperty' ),
       phetioDocumentation: StringUtils.fillIn( template, { name: 'Ears' } )
     } );
+
     this.teethVisibleProperty = createGeneVisibleProperty( genePool.teethGene, options.teethVisible, {
       tandem: options.tandem.createTandem( 'teethVisibleProperty' ),
       phetioDocumentation: StringUtils.fillIn( template, { name: 'Teeth' } )
     } );
   }
 
-  /**
-   * @public
-   */
-  dispose() {
+  public dispose(): void {
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
   }
 }
 
 naturalSelection.register( 'GenesVisibilityManager', GenesVisibilityManager );
-export default GenesVisibilityManager;
