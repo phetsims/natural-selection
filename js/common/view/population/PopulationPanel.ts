1a3214f2090e9197911c8a160b46d51e0aff49a6
Chris Malley <cmalley@pixelzoom.com>
2022-11-10 12:19:25 -0700

    convert to TypeScript

--- js/common/view/population/PopulationPanel.ts
+++ js/common/view/population/PopulationPanel.ts
@@ -1,6 +1,5 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * PopulationPanel is the panel that contains controls for the 'Population' graph.
  *
@@ -8,36 +7,35 @@
  */
 
 import merge from '../../../../../phet-core/js/merge.js';
+import { EmptySelfOptions, optionize4 } from '../../../../../phet-core/js/optionize.js';
 import { AlignBox, AlignGroup, HSeparator, Text, VBox } from '../../../../../scenery/js/imports.js';
 import Checkbox from '../../../../../sun/js/Checkbox.js';
-import Tandem from '../../../../../tandem/js/Tandem.js';
 import naturalSelection from '../../../naturalSelection.js';
 import NaturalSelectionStrings from '../../../NaturalSelectionStrings.js';
 import Gene from '../../model/Gene.js';
 import PopulationModel from '../../model/PopulationModel.js';
 import NaturalSelectionColors from '../../NaturalSelectionColors.js';
 import NaturalSelectionConstants from '../../NaturalSelectionConstants.js';
-import NaturalSelectionPanel from '../NaturalSelectionPanel.js';
+import NaturalSelectionPanel, { NaturalSelectionPanelOptions } from '../NaturalSelectionPanel.js';
 import PopulationAlleleCheckbox from './PopulationAlleleCheckbox.js';
 import PopulationLegendCheckbox from './PopulationLegendCheckbox.js';
 
-class PopulationPanel extends NaturalSelectionPanel {
+type SelfOptions = EmptySelfOptions;
 
-  /**
-   * @param {PopulationModel} populationModel
-   * @param {Object} [options]
-   */
-  constructor( populationModel, options ) {
+type PopulationPanelOptions = SelfOptions & NaturalSelectionPanelOptions;
 
-    assert && assert( populationModel instanceof PopulationModel, 'invalid populationModel' );
+export default class PopulationPanel extends NaturalSelectionPanel {
 
-    options = merge( {
-      fixedWidth: 100,
-      xMargin: 0,
+  private readonly alleleCheckboxes: PopulationAlleleCheckbox[];
 
-      // phet-io
-      tandem: Tandem.REQUIRED
-    }, NaturalSelectionConstants.PANEL_OPTIONS, options );
+  public constructor( populationModel: PopulationModel, providedOptions?: PopulationPanelOptions ) {
+
+    const options = optionize4<PopulationPanelOptions, SelfOptions, NaturalSelectionPanelOptions>()(
+      {}, NaturalSelectionConstants.PANEL_OPTIONS, {
+
+        // NaturalSelectionPanelOptions
+        fixedWidth: 100
+      }, providedOptions );
 
     const furGene = populationModel.genePool.furGene;
     const earsGene = populationModel.genePool.earsGene;
@@ -109,7 +107,7 @@ class PopulationPanel extends NaturalSelectionPanel {
     // Dilate the pointer areas to fill vertical space between the checkboxes.
     // See https://github.com/phetsims/natural-selection/issues/173
     const xDilation = 8;
-    const yDilation = NaturalSelectionConstants.VBOX_OPTIONS.spacing / 2;
+    const yDilation = NaturalSelectionConstants.VBOX_OPTIONS.spacing! / 2;
     checkboxes.forEach( checkbox => {
       checkbox.touchArea = checkbox.localBounds.dilatedXY( xDilation, yDilation );
       checkbox.mouseArea = checkbox.localBounds.dilatedXY( xDilation, yDilation );
@@ -141,7 +139,7 @@ class PopulationPanel extends NaturalSelectionPanel {
     dataProbeCheckbox.mouseArea = dataProbeCheckbox.localBounds.dilatedXY( xDilation, yDilation );
 
     const content = new VBox( merge( {}, NaturalSelectionConstants.VBOX_OPTIONS, {
-      preferredWidth: options.fixedWidth - ( 2 * options.xMargin ),
+      preferredWidth: options.fixedWidth! - ( 2 * options.xMargin ),
       children: [
         checkboxesVBox,
         separator,
@@ -151,40 +149,29 @@ class PopulationPanel extends NaturalSelectionPanel {
 
     super( content, options );
 
-    // @private {PopulationAlleleCheckbox[]}
     this.alleleCheckboxes = alleleCheckboxes;
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
     super.dispose();
   }
 
   /**
    * Sets visibility of the UI components related to a specific gene.
-   * @param {Gene} gene
-   * @param {boolean} visible
-   * @public
    */
-  setGeneVisible( gene, visible ) {
-    assert && assert( gene instanceof Gene, 'invalid gene' );
-    assert && assert( typeof visible === 'boolean', 'invalid visible' );
+  public setGeneVisible( gene: Gene, visible: boolean ): void {
 
     // Checkbox for the normal allele
-    const normalCheckbox = _.find( this.alleleCheckboxes, checkbox => ( checkbox.allele === gene.normalAllele ) );
+    const normalCheckbox = _.find( this.alleleCheckboxes, checkbox => ( checkbox.allele === gene.normalAllele ) )!;
     assert && assert( normalCheckbox, `normalCheckbox not found for ${gene.normalAllele.nameProperty.value} allele` );
     normalCheckbox.visible = visible;
 
     // Checkbox for the mutant allele
-    const mutantCheckbox = _.find( this.alleleCheckboxes, checkbox => ( checkbox.allele === gene.mutantAllele ) );
+    const mutantCheckbox = _.find( this.alleleCheckboxes, checkbox => ( checkbox.allele === gene.mutantAllele ) )!;
     assert && assert( normalCheckbox, `mutantCheckbox not found for ${gene.mutantAllele.nameProperty.value} allele` );
     mutantCheckbox.visible = visible;
   }
 }
 
 naturalSelection.register( 'PopulationPanel', PopulationPanel );
-export default PopulationPanel;
