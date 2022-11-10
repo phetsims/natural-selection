5395d64ff5c208bd1bcf11ee995579ed3497168e
pixelzoom <cmalley@pixelzoom.com>
2022-11-10 00:01:43 -0700

    convert to TypeScript

--- js/common/view/BunnyImageMap.ts
+++ js/common/view/BunnyImageMap.ts
@@ -1,6 +1,5 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * BunnyImageMap maps a bunny to a visual representation that corresponds to its phenotype.
  * The visual representation can be a Sprite (for use in OrganismSprites) or a Node (for use in the Pedigree graph).
@@ -8,8 +7,8 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import { AlignBox, AlignGroup, Image, Node, Sprite } from '../../../../scenery/js/imports.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import { AlignBox, AlignBoxOptions, AlignGroup, Image, Node, NodeOptions, NodeTransformOptions, Sprite } from '../../../../scenery/js/imports.js';
 import bunnyBrownFurFloppyEarsLongTeeth_png from '../../../images/bunnyBrownFurFloppyEarsLongTeeth_png.js';
 import bunnyBrownFurFloppyEarsShortTeeth_png from '../../../images/bunnyBrownFurFloppyEarsShortTeeth_png.js';
 import bunnyBrownFurStraightEarsLongTeeth_png from '../../../images/bunnyBrownFurStraightEarsLongTeeth_png.js';
@@ -22,34 +21,39 @@ import naturalSelection from '../../naturalSelection.js';
 import Bunny from '../model/Bunny.js';
 import BunnySpriteImage from './environment/BunnySpriteImage.js';
 
-class BunnyImageMap {
+// Keys for all maps herein. See getKey().
+type Key =
+  'true-true-true' | 'true-true-false' | 'true-false-true' | 'true-false-false' |
+  'false-true-true' | 'false-true-false' | 'false-false-true' | 'false-false-false';
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
+export default class BunnyImageMap {
 
-    // Maps a bunny's phenotype to an HTMLImageElement. See getKey for how the key is formed.
+  // Sprites for each bunny phenotype, used in OrganismSprites. Uses the same keys as const imageMap.
+  private readonly spriteMap: Map<Key, Sprite>;
+
+  // Nodes for each bunny phenotype, used in the Pedigree graph. Uses the same keys as const imageMap.
+  // All of these Nodes have the same origin and effective size.
+  private readonly nodeMap: Map<Key, Node>;
+
+  public constructor() {
+
+    // Maps a bunny's phenotype to an HTMLImageElement.
     // This map is not static because the images need to be loaded before they can be used by SpriteImage.
-    const imageMap = {
+    const imageMap = new Map<Key, HTMLImageElement>();
+    imageMap.set( 'true-true-true', bunnyWhiteFurStraightEarsShortTeeth_png );
+    imageMap.set( 'true-true-false', bunnyWhiteFurStraightEarsLongTeeth_png );
+    imageMap.set( 'true-false-true', bunnyWhiteFurFloppyEarsShortTeeth_png );
+    imageMap.set( 'true-false-false', bunnyWhiteFurFloppyEarsLongTeeth_png );
+    imageMap.set( 'false-true-true', bunnyBrownFurStraightEarsShortTeeth_png );
+    imageMap.set( 'false-true-false', bunnyBrownFurStraightEarsLongTeeth_png );
+    imageMap.set( 'false-false-true', bunnyBrownFurFloppyEarsShortTeeth_png );
+    imageMap.set( 'false-false-false', bunnyBrownFurFloppyEarsLongTeeth_png );
+    assert && assert( imageMap.size === 8, 'imageMap is incomplete' );
 
-      // key: value
-      'true-true-true': bunnyWhiteFurStraightEarsShortTeeth_png,
-      'true-true-false': bunnyWhiteFurStraightEarsLongTeeth_png,
-      'true-false-true': bunnyWhiteFurFloppyEarsShortTeeth_png,
-      'true-false-false': bunnyWhiteFurFloppyEarsLongTeeth_png,
-      'false-true-true': bunnyBrownFurStraightEarsShortTeeth_png,
-      'false-true-false': bunnyBrownFurStraightEarsLongTeeth_png,
-      'false-false-true': bunnyBrownFurFloppyEarsShortTeeth_png,
-      'false-false-false': bunnyBrownFurFloppyEarsLongTeeth_png
-    };
-    assert && assert( _.keys( imageMap ).length === 8, 'imageMap is incomplete' );
-    assert && assert( _.every( _.keys( imageMap ), key => key.match( /(true|false)-(true|false)-(true|false)/ ) ),
-      'imageMap has an invalid key' );
-
-    // @private {Object.<string,Sprite>} - Sprites for each bunny phenotype, used in OrganismSprites. Uses the same keys
-    // as imageMap.
-    this.spriteMap = _.mapValues( imageMap, image => new Sprite( new BunnySpriteImage( image ) ) );
+    // Create a Sprite for each entry in imageMap.
+    this.spriteMap = new Map<Key, Sprite>();
+    imageMap.forEach( ( htmlImageElement, key ) =>
+      this.spriteMap.set( key, new Sprite( new BunnySpriteImage( htmlImageElement ) ) ) );
 
     // Hit test on non-transparent pixels, to make it easier to select overlapping bunnies.
     // See https://github.com/phetsims/natural-selection/issues/63
@@ -58,40 +62,35 @@ class BunnyImageMap {
     };
 
     // Make all Nodes have the same origin and effective dimensions.
-    const alignBoxOptions = {
+    const alignBoxOptions: AlignBoxOptions = {
       xAlign: 'center',
       yAlign: 'bottom',
       group: new AlignGroup()
     };
 
-    // @private {Object.<string,Node>} - Nodes for each bunny phenotype, used in the Pedigree graph. Uses the same keys
-    // as imageMap. All of these Nodes have the same origin and effective size.
-    this.nodeMap = _.mapValues( imageMap, image => new AlignBox( new Image( image, imageOptions ), alignBoxOptions ) );
+    // Create a Node for each entry in imageMap.
+    this.nodeMap = new Map<Key, Node>();
+    imageMap.forEach( ( htmlImageElement, key ) =>
+      this.nodeMap.set( key, new AlignBox( new Image( htmlImageElement, imageOptions ), alignBoxOptions ) ) );
   }
 
   /**
    * Gets the complete set of Sprites for all bunny phenotypes.
-   * @returns {Sprite[]}
-   * @public
    */
-  getSprites() {
-    return _.values( this.spriteMap );
+  public getSprites(): Sprite[] {
+    return Array.from( this.spriteMap.values() );
   }
 
   /**
    * Gets the Sprite that matches a bunny's phenotype, for use in OrganismSprites.
-   * @param {Bunny} bunny
-   * @returns {Sprite}
-   * @public
    */
-  getSprite( bunny ) {
-    assert && assert( bunny instanceof Bunny, 'invalid bunny' );
+  public getSprite( bunny: Bunny ): Sprite {
 
     // Create the key that corresponds to the bunny's phenotype.
     const key = getKey( bunny );
 
     // Look up the Sprite in the map.
-    const sprite = this.spriteMap[ key ];
+    const sprite = this.spriteMap.get( key )!;
     assert && assert( sprite, `no sprite found for key ${key}` );
 
     return sprite;
@@ -101,25 +100,18 @@ class BunnyImageMap {
    * Gets the Node that matches a bunny's phenotype, for use in the Pedigree graph. Since this Node is used in multiple
    * places in scenery's DAG, the Node is wrapped with another Node, so that it can be transformed without causing
    * problems.
-   * @param {Bunny} bunny
-   * @param {Object} [options] - applied to Node wrapper
-   * @returns {Node}
-   * @public
    */
-  getNode( bunny, options ) {
+  public getNode( bunny: Bunny, providedOptions?: NodeTransformOptions ): Node {
 
-    assert && assert( bunny instanceof Bunny, 'invalid bunny' );
-
-    options = merge( {}, options );
+    const options = combineOptions<NodeOptions>( {}, providedOptions );
 
     // Create the key that corresponds to the bunny's phenotype.
     const key = getKey( bunny );
 
     // Look up the Node in the map.
-    const node = this.nodeMap[ key ];
+    const node = this.nodeMap.get( key )!;
     assert && assert( node, `no Node found for key ${key}` );
 
-    assert && assert( !options.children, 'getWrappedNode sets children' );
     options.children = [ node ];
 
     return new Node( options );
@@ -130,13 +122,9 @@ class BunnyImageMap {
  * Gets the key that corresponds to a bunny's phenotype. Instead of a big if-then-else statement for each
  * permutation of gene, this implementation converts the phenotype to a string key. The key pattern is
  * '{{hasWhiteFur}}-{{hasStraightEars}}-{{hasShortTeeth}}', where the value for each placeholder is 'true' or 'false'.
- * @param {Bunny} bunny
- * @returns {string}
- * @private
  */
-function getKey( bunny ) {
+function getKey( bunny: Bunny ): Key {
   return `${bunny.phenotype.hasWhiteFur()}-${bunny.phenotype.hasStraightEars()}-${bunny.phenotype.hasShortTeeth()}`;
 }
 
 naturalSelection.register( 'BunnyImageMap', BunnyImageMap );
-export default BunnyImageMap;
