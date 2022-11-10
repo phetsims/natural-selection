3fafb51a3ae1f7577b664163788cd7a0d18579c9
phet-dev <phetadmin@gmail.com>
2022-11-10 04:28:58 -0700

    update copyright dates from daily grunt work

--- js/common/view/BunnySelectionRectangle.ts
+++ js/common/view/BunnySelectionRectangle.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * BunnySelectionRectangle is the rectangle that appears around the selected bunny in the environment and
--- js/common/view/GenesVisibilityManager.ts
+++ js/common/view/GenesVisibilityManager.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * GenesVisibilityManager manages the visibility of UI components that are related to specific genes.
