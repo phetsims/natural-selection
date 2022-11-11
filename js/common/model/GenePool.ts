42583c941243b25a772fa260ddc2dc5e794a080f
pixelzoom <cmalley@pixelzoom.com>
2022-11-11 16:58:29 -0700

    convert to TypeScript

--- js/common/model/GenePool.ts
+++ js/common/model/GenePool.ts
@@ -1,6 +1,5 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * GenePool is the pool of genes for the bunny population.
  * There is one instance of GenePool per screen.
@@ -8,70 +7,57 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import Tandem from '../../../../tandem/js/Tandem.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
 import naturalSelection from '../../naturalSelection.js';
 import Allele from './Allele.js';
 import Gene from './Gene.js';
 
+type SelfOptions = EmptySelfOptions;
+
+type GenePoolOptions = SelfOptions & PickRequired<PhetioObjectOptions, 'tandem'>;
+
 export default class GenePool {
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
+  public readonly furGene: Gene;
+  public readonly earsGene: Gene;
+  public readonly teethGene: Gene;
 
-    options = merge( {
+  // For situations where it's possible to iterate over genes, the order here determines the order of UI components.
+  public readonly genes: Gene[];
 
-      // phet-io
-      tandem: Tandem.REQUIRED
-    }, options );
+  public constructor( providedOptions: GenePoolOptions ) {
 
-    // @public (read-only) {Gene}
+    const options = providedOptions;
+
     this.furGene = Gene.createFurGene( options.tandem.createTandem( 'furGene' ) );
-
-    // @public (read-only) {Gene}
     this.earsGene = Gene.createEarsGene( options.tandem.createTandem( 'earsGene' ) );
-
-    // @public (read-only) {Gene}
     this.teethGene = Gene.createTeethGene( options.tandem.createTandem( 'teethGene' ) );
-
-    // @public (read-only) {Gene[]} for situations where it's possible to iterate over genes
-    // When we're able to iterate to create the UI, the order here determines the order of UI components.
     this.genes = [ this.furGene, this.earsGene, this.teethGene ];
   }
 
-  /**
-   * @public
-   */
-  reset() {
+  public dispose(): void {
+    assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
+  }
+
+  public reset(): void {
     this.genes.forEach( gene => gene.reset() );
   }
 
   /**
    * Resets mutationComingProperty for all genes in the pool. This is called after a mating cycle has completed, and
    * mutations have been applied.
-   * @public
    */
-  resetMutationComing() {
+  public resetMutationComing(): void {
     this.genes.forEach( gene => gene.mutationComingProperty.reset() );
   }
 
   /**
-   * @public
-   */
-  dispose() {
-    assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
-  }
-
-  /**
    * Is the specified allele a recessive mutation?
-   * @param {Allele} allele
-   * @returns {boolean}
-   * @public
    */
-  isRecessiveMutation( allele ) {
-    assert && assert( allele instanceof Allele, 'invalid allele' );
+  public isRecessiveMutation( allele: Allele ): boolean {
 
     let isRecessiveMutation = false;
     for ( let i = 0; i < this.genes.length && !isRecessiveMutation; i++ ) {
@@ -84,11 +70,9 @@ export default class GenePool {
   /**
    * Gets the dependencies on dynamic strings that are used to derive the abbreviations for genes in the pool.
    * These strings may be changed via PhET-iO, or by changing the global localeProperty.
-   * @returns {Property.<*>[]}
-   * @public
    */
-  getGenotypeAbbreviationStringDependencies() {
-    const dependencies = [];
+  public getGenotypeAbbreviationStringDependencies(): TReadOnlyProperty<string>[] {
+    const dependencies: TReadOnlyProperty<string>[] = [];
     this.genes.forEach( gene => {
       dependencies.push( gene.dominantAbbreviationTranslatedProperty );
       dependencies.push( gene.recessiveAbbreviationTranslatedProperty );
