957fef9f5b7835e55fffc84d17de379931329d90
pixelzoom <cmalley@pixelzoom.com>
2022-11-11 16:18:14 -0700

    // @ts-nocheck

--- js/common/model/WolfCollection.js
+++ js/common/model/WolfCollection.js
@@ -1,5 +1,6 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * WolfCollection is the collection of Wolf instances, with methods for managing that collection.
  * It encapsulates WolfGroup (the PhetioGroup), hiding it from the rest of the sim.
