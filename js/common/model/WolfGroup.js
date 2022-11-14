e12a18fc1bc575401d7c6773bc35fedf8f697859
pixelzoom <cmalley@pixelzoom.com>
2022-11-14 13:51:18 -0700

    // @ts-nocheck

--- js/common/model/WolfGroup.js
+++ js/common/model/WolfGroup.js
@@ -1,5 +1,6 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * WolfGroup is the PhetioGroup for Wolf.  It manages dynamic instances of Wolf, as required by PhET-iO.
  * All Wolf instances are created and disposed via this group.
