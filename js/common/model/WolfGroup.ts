adaefa3b0f663bcb222f9b5d886c0f7b10bcfd2a
pixelzoom <cmalley@pixelzoom.com>
2022-11-14 14:03:51 -0700

    convert to TypeScript, https://github.com/phetsims/natural-selection/issues/326

--- js/common/model/WolfGroup.ts
+++ js/common/model/WolfGroup.ts
@@ -1,6 +1,5 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * WolfGroup is the PhetioGroup for Wolf.  It manages dynamic instances of Wolf, as required by PhET-iO.
  * All Wolf instances are created and disposed via this group.
@@ -8,41 +7,38 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import PhetioGroup from '../../../../tandem/js/PhetioGroup.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import PhetioGroup, { PhetioGroupOptions } from '../../../../tandem/js/PhetioGroup.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import naturalSelection from '../../naturalSelection.js';
 import EnvironmentModelViewTransform from './EnvironmentModelViewTransform.js';
 import Wolf from './Wolf.js';
 
-export default class WolfGroup extends PhetioGroup {
+type SelfOptions = EmptySelfOptions;
 
-  /**
-   * @param {EnvironmentModelViewTransform} modelViewTransform
-   * @param {Object} [options]
-   */
-  constructor( modelViewTransform, options ) {
+type WolfGroupOptions = SelfOptions & PickRequired<PhetioGroupOptions, 'tandem'>;
 
-    assert && assert( modelViewTransform instanceof EnvironmentModelViewTransform, 'invalid modelViewTransform' );
+export default class WolfGroup extends PhetioGroup<Wolf> {
 
-    options = merge( {
+  public constructor( modelViewTransform: EnvironmentModelViewTransform, providedOptions: WolfGroupOptions ) {
 
-      // phet-io
-      tandem: Tandem.REQUIRED,
+    const options = optionize<WolfGroupOptions, SelfOptions, PhetioGroupOptions>()( {
+
+      // PhetioGroupOptions
       phetioType: PhetioGroup.PhetioGroupIO( Wolf.WolfIO ),
       phetioDocumentation: 'manages dynamic PhET-iO elements of type Wolf'
-    }, options );
+    }, providedOptions );
 
     /**
      * Called to instantiate a Wolf. Note that modelViewTransform is passed via closure, so we don't
      * have to create it as part of defaultArguments, and don't have to deal with serializing it in WolfIO.
-     * @param {Tandem} tandem - PhetioGroup requires tandem to be the first param
-     * @returns {Wolf}
+     * @param tandem - PhetioGroup requires tandem to be the first param
      */
-    const createElement = tandem => new Wolf( modelViewTransform, tandem );
+    const createElement = ( tandem: Tandem ) => new Wolf( modelViewTransform, tandem );
 
     // defaultArguments, passed to createElement during API harvest
-    const defaultArguments = [];
+    const defaultArguments: [] = [];
 
     super( createElement, defaultArguments, options );
   }
