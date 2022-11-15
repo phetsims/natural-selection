3b87afbefef79ac861cba33c4ac32dd81913150a
pixelzoom <cmalley@pixelzoom.com>
2022-11-14 21:12:45 -0700

    // @ts-nocheck

--- js/common/model/BunnyCollection.js
+++ js/common/model/BunnyCollection.js
@@ -1,5 +1,6 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * BunnyCollection is the collection of Bunny instances, with methods for managing that collection.
  * It encapsulates BunnyGroup (the PhetioGroup), hiding it from the rest of the sim.
