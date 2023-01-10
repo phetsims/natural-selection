74b2ef54c6ec056f0107b42a87414f5a25075557
phet-dev <phetadmin@gmail.com>
2023-01-10 04:48:02 -0700

    update copyright dates from daily grunt work

--- js/common/view/HatchingRectangle.ts
+++ js/common/view/HatchingRectangle.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * HatchingRectangle is a Rectangle that appears as if it's filled with a hatching pattern.
