a6e08c071b18431e28f37b306a6917d062945e61
phet-dev <phetadmin@gmail.com>
2023-02-16 04:36:17 -0700

    update copyright dates from daily grunt work

--- js/common/model/NaturalSelectionModel.ts
+++ js/common/model/NaturalSelectionModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * NaturalSelectionModel is the base class model for all screens.
