b0b61ec8523002e869593e75624e8d001d841109
phet-dev <phetadmin@gmail.com>
2023-06-16 03:32:31 -0600

    update copyright dates from daily grunt work

--- js/common/view/pedigree/PedigreeGraphNode.ts
+++ js/common/view/pedigree/PedigreeGraphNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * PedigreeGraphNode displays the pedigree for an individual. Origin at bottom center.
