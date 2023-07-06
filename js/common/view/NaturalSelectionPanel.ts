d043fc1588a9b304052fd3826ab3f58f2cdc9e5a
phet-dev <phetadmin@gmail.com>
2023-07-06 03:33:25 -0600

    update copyright dates from daily grunt work

--- js/common/view/NaturalSelectionPanel.ts
+++ js/common/view/NaturalSelectionPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * NaturalSelectionPanel is a specialization of Panel that provides a more convenient API for creating a
