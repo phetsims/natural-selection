98881334fdd262590febf62d1a0f9b12c37e0bd1
pixelzoom <cmalley@pixelzoom.com>
2023-07-07 16:06:16 -0600

    doc

--- js/common/model/BunnyCounts.ts
+++ js/common/model/BunnyCounts.ts
@@ -147,8 +147,8 @@ export default class BunnyCounts {
    * BunnyCountsIO implements 'Data type serialization', as described in the Serialization section of
    * https://github.com/phetsims/phet-io/blob/master/doc/phet-io-instrumentation-technical-guide.md#serialization
    * Data type serialization is appropriate because BunnyCount itself is not a PhetioObject. Its role is as
-   * a data type - similar to number, string, or Vector2. Various Properties have a value of type BunnyCount.
-   * Search for BunnyCountsIO to identify those Properties.
+   * a data type - similar to number, string, or Vector2. Various fields and Properties have a value of type BunnyCount.
+   * Search for BunnyCountsIO to identify them.
    */
   public static readonly BunnyCountsIO = new IOType<BunnyCounts, BunnyCountsStateObject>( 'BunnyCountsIO', {
     valueType: BunnyCounts,
