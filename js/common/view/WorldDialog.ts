60f1f77d8e1fda6f663611943081331375b04474
phet-dev <phetadmin@gmail.com>
2023-07-23 03:33:01 -0600

    update copyright dates from daily grunt work

--- js/common/view/WorldDialog.ts
+++ js/common/view/WorldDialog.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * WorldDialog is displayed when bunnies have exceeded their maximum population.
