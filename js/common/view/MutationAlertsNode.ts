4306aee7353f2e6768731f1f3f4e17918b731775
pixelzoom <cmalley@pixelzoom.com>
2023-08-01 13:06:51 -0600

    dynamically position "Mutation Coming" alerts, https://github.com/phetsims/natural-selection/issues/349

--- js/common/view/MutationAlertsNode.ts
+++ js/common/view/MutationAlertsNode.ts
@@ -14,6 +14,7 @@ import AddMutationsPanel from './AddMutationsPanel.js';
 import MutationComingNode from './MutationComingNode.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import Multilink from '../../../../axon/js/Multilink.js';
 
 const X_OFFSET = -5;
 
@@ -41,21 +42,23 @@ export default class MutationAlertsNode extends Node {
 
     super( options );
 
-    // Alerts point at rows in the Add Mutations panel. Since rows can be dynamically hidden via PhET-iO, manage
-    // the logistics of that here. unlink is not necessary.
-    addMutationsPanel.boundsProperty.link( () => {
-      mutationComingNodes.forEach( mutationComingNode => {
+    // Alerts point at rows in the Add Mutations panel. Since rows can be dynamically hidden via PhET-iO, dynamically
+    // position each alert to the left of its associated row.
+    mutationComingNodes.forEach( mutationComingNode => {
+      const row = addMutationsPanel.getRow( mutationComingNode.gene );
+      Multilink.multilink(
+        [ row.visibleProperty, addMutationsPanel.boundsProperty ],
+        ( rowVisible, addMutationsPanelBounds ) => {
 
-        // Position the alert to the left of its associated rows.
-        const row = addMutationsPanel.getRow( mutationComingNode.gene );
-        const globalPoint = row.parentToGlobalPoint( new Vector2( row.left, row.centerY ) ).addXY( X_OFFSET, 0 );
-        mutationComingNode.rightCenter = mutationComingNode.globalToParentPoint( globalPoint );
+          // To the left of its associated row
+          const globalPoint = row.parentToGlobalPoint( new Vector2( row.left, row.centerY ) ).addXY( X_OFFSET, 0 );
+          mutationComingNode.rightCenter = mutationComingNode.globalToParentPoint( globalPoint );
 
-        // If the row was made invisible for a gene that has a mutation was coming, cancel the mutation.
-        if ( !row.visible && mutationComingNode.gene.mutationComingProperty.value ) {
-          mutationComingNode.gene.cancelMutation();
-        }
-      } );
+          // If the row was made invisible for a gene that has a mutation coming, cancel the mutation.
+          if ( !rowVisible && mutationComingNode.gene.mutationComingProperty.value ) {
+            mutationComingNode.gene.cancelMutation();
+          }
+        } );
     } );
   }
 }
