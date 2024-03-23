86453feb739189a86adbb143c957dfdf22ad5741
phet-dev <phetadmin@gmail.com>
2024-03-23 03:23:46 -0600

    update copyright dates from daily grunt work

--- js/common/model/Gene.ts
+++ js/common/model/Gene.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2023, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 
 /**
  * Gene is the basic physical and functional unit of heredity that is transferred from a parent to its offspring,
