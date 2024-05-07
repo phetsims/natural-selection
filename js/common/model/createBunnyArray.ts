69edfd7dbf7f7d26edd085d0e79684a1ecffe571
phet-dev <phetadmin@gmail.com>
2024-05-07 03:27:58 -0600

    update copyright dates from daily grunt work

--- js/common/model/createBunnyArray.ts
+++ js/common/model/createBunnyArray.ts
@@ -1,4 +1,4 @@
-// Copyright 2020-2023, University of Colorado Boulder
+// Copyright 2020-2024, University of Colorado Boulder
 
 /**
  * createBunnyArray creates an observable Array that has counts for each phenotype.
